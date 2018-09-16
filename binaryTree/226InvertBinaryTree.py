e8d7a2feb1ebd3bbaf9d9d54274ed5969d76cb38
lo-tp <regesteraccount@hotmail.com>
2018-09-16 09:49:44 +0800

    226 done

---
+++ binaryTree/226InvertBinaryTree.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def invertTreeRecursive(self, root):
+        if root:
+            root.left=self.invertTree(root.right)
+            root.right=self.invertTree(root.left)
+        return root
+    def invertTreeDFS(self, root):
+        s=[]
+        w=root
+        while s or root:
+            if root:
+                s.append(root)
+                root=root.left
+            else:
+                root=s.pop()
+                k=root.left
+                root.left=root.right
+                root.right=k
+                root=root.left
+        return w
+    def invertTreeBFS(self, root):
+        s=[root]
+        w=[]
+        ret=root
+        while s:
+            while s:
+                t=s.pop()
+                if t:
+                    k=t.left
+                    t.left=t.right
+                    t.right=k
+                    w.append(t.left)
+                    w.append(t.right)
+            k=s
+            s=w
+            w=k
+        return ret
--- other/2AddTwoNumbers/0.py
+++ other/2AddTwoNumbers/0.py
@@ -1,27 +1,40 @@
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        ret,addone=l1,False
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        ret=l1
         addOne=False
-        while l1.next and l2:
-            k=l1.val+l2.val
+        while l1.next and l2.next:
+            l1.val+=l2.val
             if addOne:
-              k+=1
-            addOne = k>=10
-            l1.val=k%10
+                l1.val+=1
+            if l1.val>9:
+                l1.val%=10
+                addOne=True
+            else:
+                addOne=False
             l1=l1.next
             l2=l2.next
-        if addOne:
-            l1.val=l1.val+1
-        if l2:
-            l1.val+=l2.val
+        if l2.next:
             l1.next=l2.next
-        addOne=l1.val>=10
-        l1.val%=10
-        while l1.next and addOne:
+        l1.val+=l2.val
+        if addOne:
+            l1.val+=1
+        if l1.val>9:
+            l1.val%=10
+            addOne=True
+        else:
+            addOne=False
+        while l1 and l1.next and addOne:
             l1=l1.next
             l1.val+=1
-            addOne=l1.val>=10
-            l1.val%=10
+            if l1.val>9:
+                l1.val%=10
+                addOne=True
+            else:
+                addOne=False
         if addOne:
             l1.next=ListNode(1)
-        return ret
