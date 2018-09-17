7b450febcb7ad54151d73cfbad6e984824776ab3
lo-tp <regesteraccount@hotmail.com>
2018-09-17 12:18:08 +0800

    449 done

---
+++ other/449FindAllNumbersDisappearedInAnArray.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in nums:
+            index=abs(i)-1
+            if nums[index]>0:
+                nums[index]*=-1
+        return [i + 1 for i in range(len(nums)) if nums[i] > 0]
