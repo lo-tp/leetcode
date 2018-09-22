afc2bc73c05f78901647453b25d2fe34626089d2
lo-tp <regesteraccount@hotmail.com>
2018-09-22 14:54:11 +0800

    64 done better

--- dp/64MinimumPathSum.py
+++ dp/64MinimumPathSum.py
@@ -1,33 +1,21 @@
 class Solution(object):
     def minPathSum(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
         verticalSize=len(grid)
-        horizontalSize=len(grid[ 0 ])
-        horizontalIndex=1
-        while horizontalIndex<horizontalSize:
-            grid[0][horizontalIndex]+=grid[0][horizontalIndex-1]
-            horizontalIndex+=1
-        verticalIndex=1
+        horizontalSize=len(grid[0])
+        verticalIndex=0
         while verticalIndex<verticalSize:
-            grid[verticalIndex][0]+=grid[verticalIndex-1][0]
-            verticalIndex+=1
-        verticalIndex=1
-        while verticalIndex<verticalSize:
-            horizontalIndex=1
+            horizontalIndex=0
             while horizontalIndex<horizontalSize:
-                if grid[verticalIndex-1][horizontalIndex]<grid[verticalIndex][horizontalIndex-1]:
-                    grid[verticalIndex][horizontalIndex]+=grid[verticalIndex-1][horizontalIndex]
+                if verticalIndex==0 and horizontalIndex==0:
+                    horizontalIndex+=1
+                    continue;
+                if verticalIndex==0:
+                        grid[verticalIndex][horizontalIndex]+=grid[verticalIndex][horizontalIndex-1]
+                elif horizontalIndex==0:
+                        grid[verticalIndex][horizontalIndex]+=grid[verticalIndex-1][horizontalIndex]
                 else:
-                    grid[verticalIndex][horizontalIndex]+=grid[verticalIndex][horizontalIndex-1]
+                    grid[verticalIndex][horizontalIndex]+=min(grid[verticalIndex-1][horizontalIndex], grid[verticalIndex][horizontalIndex-1])
                 horizontalIndex+=1
             verticalIndex+=1
-        return grid[verticalSize-1][horizontalSize-1]
-s=Solution()
-print s.minPathSum([
-  [1,3,1],
-  [1,5,1],
-  [4,2,1]
-])
+        return grid[verticalSize-1][horizontalIndex-1];
+
