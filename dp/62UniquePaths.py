6b24f48e9e9042b293edbfdc51991c654a9b31a5
lo-tp <regesteraccount@hotmail.com>
2018-09-26 09:15:15 +0800

    62 done with dp

--- dp/62UniquePaths.py
+++ dp/62UniquePaths.py
@@ -10,5 +10,17 @@ class Solution(object):
         minNum=min(m,n)-1
         maxMun=max(m,n)-1
         return self.calculataPermulationNumber(maxMun+minNum)/self.calculataPermulationNumber(minNum)/self.calculataPermulationNumber(maxMun) if minNum else 1;
+    def uniquePathsDp(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        result=[[1 for i in xrange(m)] for j in xrange(n)]
+        for verticalIndex in xrange(n):
+            for horizontalIndex in xrange(m):
+                if horizontalIndex and verticalIndex:
+                    result[verticalIndex][horizontalIndex]=result[verticalIndex-1][horizontalIndex]+result[verticalIndex][horizontalIndex-1]
+        return result[n-1][m-1]
 s=Solution()
 print s.uniquePaths(3,7)
