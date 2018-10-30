ec696fcf25a9dc879afcd86112e0c06d3bc6c9eb
lo-tp <regesteraccount@hotmail.com>
2018-10-30 14:29:42 +0800

    48 refined

--- other/48RotateImage.py
+++ other/48RotateImage.py
@@ -1,30 +1,19 @@
-def finePrint(data):
-    for i in xrange(0, len(data)):
-        print data[i]
-
-def swap(data, x,y):
-    if data[x[0]][x[1]]!=data[y[0]][y[1]]:
-        data[x[0]][x[1]]=data[x[0]][x[1]]^data[y[0]][y[1]]
-        data[y[0]][y[1]]=data[x[0]][x[1]]^data[y[0]][y[1]]
-        data[x[0]][x[1]]=data[x[0]][x[1]]^data[y[0]][y[1]]
-def coordinatesCalculator(ini, length):
-    length-=1
-    ret=[ini]
+def calculateCoordinators(original, length):
+    data=[original]
     for i in xrange(0,3):
-        ret.append([ret[i][1], length-ret[i][0]])
-    return ret
+        data.append([data[i][1], length-data[i][0]])
+    return data
 
 class Solution(object):
     def rotate(self, matrix):
-        length=len(matrix)
-        size=length
-        hCoor=0
-        while length>1:
-            for vCoor in xrange(hCoor,hCoor+length-1):
-                coordinates=coordinatesCalculator([vCoor,hCoor], size)
-                swap(matrix, coordinates[0], coordinates[1])
-                swap(matrix, coordinates[0], coordinates[2])
-                swap(matrix, coordinates[0], coordinates[3])
-                # finePrint (matrix)
-            length-=2
-            hCoor+=1
+        length=len(matrix)-1
+        operationNum=length
+        startPos=0
+        while operationNum>=1:
+            for i in xrange(0, operationNum):
+                cs=calculateCoordinators([startPos, startPos+i], length)
+                matrix[cs[0][0]][cs[0][1]],matrix[cs[1][0]][cs[1][1]]=matrix[cs[1][0]][cs[1][1]],matrix[cs[0][0]][cs[0][1]]
+                matrix[cs[0][0]][cs[0][1]],matrix[cs[3][0]][cs[3][1]]=matrix[cs[3][0]][cs[3][1]],matrix[cs[0][0]][cs[0][1]]
+                matrix[cs[2][0]][cs[2][1]],matrix[cs[3][0]][cs[3][1]]=matrix[cs[3][0]][cs[3][1]],matrix[cs[2][0]][cs[2][1]]
+            startPos+=1
+            operationNum-=2
