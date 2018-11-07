34f3b9fb5630d69dc74cc3d42f16714dc81396bc
lo-tp <regesteraccount@hotmail.com>
2018-11-07 10:33:51 +0800

    5 done in better way

--- string/5LongestPalindromicSubstring.py
+++ string/5LongestPalindromicSubstring.py
@@ -1,45 +1,34 @@
 class Solution(object):
-    def manacher(self, s):
-        processedString='#'+'#'.join(list(s))+'#'
-        size=len(processedString)
-        palindromicLength=[0]*size
-        centerIndex=0
-        rightIndex=0
-        index=0
-        while index<size:
-            mirrorIndex=2*centerIndex-index
-            if mirrorIndex>=0:
-                rightLength=rightIndex-index
-                if palindromicLength[mirrorIndex]>rightLength:
-                    palindromicLength[index]=rightLength
-                else:
-                    palindromicLength[index]=palindromicLength[mirrorIndex]
-            leftEdge=index-palindromicLength[index]-1
-            rightEdge=index+palindromicLength[index]+1
-            while leftEdge>=0 and rightEdge<size and processedString[leftEdge]==processedString[rightEdge]:
-                palindromicLength[index]+=1
-                leftEdge-=1
-                rightEdge+=1
-            if index+palindromicLength[index]>rightIndex:
-                rightIndex=index+palindromicLength[index]
-                centerIndex=index
-            index+=1
-        return palindromicLength;
     def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        palindromicLength=self.manacher(s)
-        maxIndex=0
-        index=0
-        size=len(palindromicLength)
-        while index<size:
-            if palindromicLength[index]>palindromicLength[maxIndex]:
-                maxIndex=index
-            index+=1
-        realIndex=maxIndex/2
-        maxPalindromicLength=palindromicLength[maxIndex]
-        startIndex=realIndex-maxPalindromicLength/2
-        endIndex=realIndex+maxPalindromicLength/2 + maxPalindromicLength%2;
-        return s[startIndex:endIndex]
+        str = '#'+'#'.join(list(s))+'#'
+        size=len(str)
+        dp=[0]*size
+        center=0
+        right=-1
+        ret=0
+        for i in xrange(0, size):
+            if i <=right:
+                pairIndex=center*2-i
+                if i+dp[pairIndex]<=right:
+                    dp[i]=dp[pairIndex]
+                    scanL=i-dp[pairIndex]-1
+                    scanR=i+dp[pairIndex]+1
+                    while scanL>=0 and scanR<size and str[scanL]==str[scanR]:
+                            dp[i]+=1
+                            scanL-=1
+                            scanR+=1
+                else:
+                    dp[i]=right-i
+            else:
+                scanL=i-1
+                scanR=i+1
+                while scanL>=0 and scanR<size and str[scanL]==str[scanR]:
+                        dp[i]+=1
+                        scanL-=1
+                        scanR+=1
+            if i+dp[i]>right:
+                center=i
+                right=i+dp[i]
+            if dp[i]>dp[ret]:
+                ret=i
+        return str[ret-dp[ret]: ret+dp[ret]+1].replace('#','')
