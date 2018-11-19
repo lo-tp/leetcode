43c7f017508f0b3228bee73faf5718d01e96d827
lo-tp <regesteraccount@hotmail.com>
2018-11-19 08:38:59 +0800

    486 done in better way

--- dp/486PredictTheWinner.py
+++ dp/486PredictTheWinner.py
@@ -1,43 +1,20 @@
-def calculateScore(nums, s, e, add):
-    if s==e:
-        return nums[s] if add else -nums[s]
-    a=calculateScore(nums, s+1, e, not add)
-    b=calculateScore(nums, s, e-1, not add)
-    if add:
-        return max(a+nums[s], b+nums[e])
-    else:
-        return min(a-nums[s], b-nums[e])
-
-
 class Solution(object):
-    def calculateData(self, nums, s, e):
-        if s==e:
-            return nums[s]
-        if self.dp[s][e]:
-            return self.dp[s][e]
-        self.dp[s][e]=max(nums[s]-self.calculateData(nums, s+1, e),
-                          nums[e]-self.calculateData(nums, s, e-1))
-        return self.dp[s][e]
-        
-    def PredictTheWinner0(self, nums):
-        return calculateScore(nums, 0, len(nums)-1, True)>=0
+    def getScore(self, arr, start, end):
+        score=0
+        if start in self.dp:
+            if end in self.dp[start]:
+                return self.dp[start][end]
+        else:
+            self.dp[start]={}
+        if start==end:
+            score=arr[start]
+        else:
+            lScore=arr[start]-self.getScore(arr, start+1, end)
+            rScore=arr[end]-self.getScore(arr, start, end-1)
+            score=max(lScore, rScore)
+        self.dp[start][end]=score
+        return score
     def PredictTheWinner(self, nums):
-        size=len(nums)+1
-        self.dp=[[0 for k in xrange(0, size)]for i in xrange(0, size)]
-        return self.calculateData(nums, 0, len(nums)-1)>=0
-    def PredictTheWinnerWithDP(self, nums):
         size=len(nums)
-        dp=[[0 for k in xrange(0, size)]for i in xrange(0, size)]
-        for interval in xrange(0,size):
-            for startPos in xrange(0, size-interval):
-                endPos=startPos+interval
-                if endPos==startPos:
-                    dp[endPos][endPos]=nums[startPos]
-                else:
-                    dp[startPos][endPos]=max(nums[startPos]-dp[startPos+1][endPos], nums[endPos]-dp[startPos][endPos-1])
-        return dp[0][size-1]>=0
-
-s=Solution()
-print s.PredictTheWinner([1, 5, 2])
-print s.PredictTheWinner([1, 5, 233, 7])
-print s.PredictTheWinner([1163573,4225123,1034109,6416120,4401957,408968,8769389,7498770,6003151,2054050,2621821,8204739,2586055,6520977,2014732,4750306,4172182,6965656,1861876,9549339])
+        self.dp={}
+        return self.getScore(nums,0,len(nums)-1)>=0
