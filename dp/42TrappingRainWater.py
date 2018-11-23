59c8f8878f848464acf8da184de54fdfd9fb5dba
lo-tp <regesteraccount@hotmail.com>
2018-11-23 08:34:58 +0800

    42 Done in a better way

--- dp/42TrappingRainWater.py
+++ dp/42TrappingRainWater.py
@@ -1,4 +1,20 @@
 class Solution(object):
+    def trap1(self, height):
+        l,r,lMax,rMax,res=0,len(height)-1,0,0,0
+        while l<r:
+            if height[l]<height[r]:
+                if height[l]<lMax:
+                    res+=(lMax-height[l])
+                else:
+                    lMax=height[l]
+                l+=1
+            else:
+                if height[r]<rMax:
+                    res+=(rMax-height[r])
+                else:
+                    rMax=height[r]
+                r-=1
+        return res
     def trap(self, height):
         startIndex=0
         sz=len(height)
