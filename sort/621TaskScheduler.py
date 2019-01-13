59938a560e5f1aa4662df28c1f367cf91054dba7
lo-tp <regesteraccount@hotmail.com>
2019-01-13 19:59:02 +0800

    621 done in better way

--- sort/621TaskScheduler.py
+++ sort/621TaskScheduler.py
@@ -1,49 +1,46 @@
-def qc(arr, start, end):
-    m, l, r = arr[start+(end-start)/2], start, end
-    while l <= r:
-        while arr[l] > m:
-            l += 1
-        while arr[r] < m:
-            r -= 1
-        if l <= r:
-            if arr[l] != arr[r]:
-                arr[l] = arr[l] ^ arr[r]
-                arr[r] = arr[l] ^ arr[r]
-                arr[l] = arr[l] ^ arr[r]
-            l += 1
-            r -= 1
-    if l < end:
-        qc(arr, l, end)
-    if r > start:
-        qc(arr, start, r)
+def mg(arr):
+    sz = len(arr)
+    if sz > 1:
+        lIndex, rIndex, m = 0, 0, sz/2
+        l, r = arr[:m], arr[m:]
+        index, lSz, rSz = 0, len(l), len(r)
+        mg(l)
+        mg(r)
+        while lIndex < lSz and rIndex < rSz:
+            if l[lIndex] < r[rIndex]:
+                arr[index] = r[rIndex]
+                rIndex += 1
+            else:
+                arr[index] = l[lIndex]
+                lIndex += 1
+            index += 1
+        while lIndex < lSz:
+            arr[index] = l[lIndex]
+            index += 1
+            lIndex += 1
+        while rIndex < rSz:
+            arr[index] = r[rIndex]
+            index += 1
+            rIndex += 1
 
 
 class Solution(object):
     def leastInterval(self, tasks, n):
-        data = {}
+        offset, data = ord('A'), [0]*26
         for i in tasks:
-            if i in data:
-                data[i] += 1
-            else:
-                data[i] = 1
-        data = data.values()
-        res, sz = 0, len(data)
-        if sz:
-            qc(data, 0, sz-1)
-            while data[0]:
-                i = 1
-                data[0] -= 1
+            data[ord(i)-ord('A')] += 1
+        mg(data)
+        res = 0
+        while data[0]:
+            index = 0
+            while index <= n:
+                if index < 25 and data[index]:
+                    data[index] -= 1
+                elif data[0] == 0:
+                    break
                 res += 1
-                while i <= n:
-                    if i < sz and data[i]:
-                        data[i] -= 1
-                        res += 1
-                    elif data[0]:
-                        res += 1
-                    else:
-                        break
-                    i += 1
-                qc(data, 0, sz-1)
+                index += 1
+            mg(data)
         return res
 
 
