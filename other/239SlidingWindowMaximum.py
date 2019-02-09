e0f737c637a95d2aefe7370a56c20ad930a6938f
lo-tp <regesteraccount@hotmail.com>
2019-02-09 12:29:37 +0800

    239 done with monoqueue

--- other/239SlidingWindowMaximum.py
+++ other/239SlidingWindowMaximum.py
@@ -1,8 +1,30 @@
 from sys import maxint
+from collections import deque
+
+
+class Mq:
+    def __init__(self):
+        self.q = deque()
+
+    def pop(self):
+        if self.q[0][1]:
+            self.q[0][1] -= 1
+        else:
+            self.q.popleft()
+
+    def getMax(self):
+        return self.q[0][0]
+
+    def push(self, element):
+        count = 0
+        while len(self.q) and self.q[-1][0] < element:
+            count += self.q[-1][1]+1
+            self.q.pop()
+        self.q.append([element, count])
 
 
 class Solution(object):
-    def maxSlidingWindow(self, nums, k):
+    def maxSlidingWindowBruteForce(self, nums, k):
         res, sz = [], len(nums)
         if sz and k:
             for start in xrange(0, sz-k+1):
@@ -10,4 +32,16 @@ class Solution(object):
                 for w in xrange(start, start+k):
                     m = max(m, nums[w])
                 res.append(m)
+        return res
+
+    def maxSlidingWindow(self, nums, k):
+        res, sz = [], len(nums)
+        if sz and k:
+            mq = Mq()
+            for i in nums[:k-1]:
+                mq.push(i)
+            for i in nums[k-1:]:
+                mq.push(i)
+                res.append(mq.getMax())
+                mq.pop()
         return res
