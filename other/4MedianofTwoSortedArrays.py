23f0af688d1e94e06f30a8b393b9e7a3790eb236
lo-tp <regesteraccount@hotmail.com>
2019-02-14 10:23:51 +0800

    4 done with a better way

--- other/4MedianofTwoSortedArrays.py
+++ other/4MedianofTwoSortedArrays.py
@@ -21,3 +21,27 @@ class Solution(object):
         else:
             return float(arr[sz/2]+arr[sz/2-1])/2
 
+    def findMedianSortedArraysBetter(self, nums1, nums2):
+        arr = []
+        lIndex, rIndex, lSz, rSz = 0, 0, len(nums1), len(nums2)
+        sz = (lSz+rSz)/2+1
+        while sz and lIndex < lSz and rIndex < rSz:
+            if nums1[lIndex] < nums2[rIndex]:
+                arr.append(nums1[lIndex])
+                lIndex += 1
+            else:
+                arr.append(nums2[rIndex])
+                rIndex += 1
+            sz -= 1
+        while sz and lIndex < lSz:
+            arr.append(nums1[lIndex])
+            lIndex += 1
+            sz -= 1
+        while sz and rIndex < rSz:
+            arr.append(nums2[rIndex])
+            rIndex += 1
+            sz -= 1
+        if (lSz+rSz) % 2:
+            return arr[-1]
+        else:
+            return float((arr[-1]+arr[-2]))/2
