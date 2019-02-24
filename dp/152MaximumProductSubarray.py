a057e761f68051014c5fb32674fb1eda4aa12147
lo-tp <regesteraccount@hotmail.com>
2019-02-24 09:46:57 +0800

    152 done with dp

--- dp/152MaximumProductSubarray.py
+++ dp/152MaximumProductSubarray.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def maxProduct(self, nums):
+    def maxProductTLE(self, nums):
         sz = len(nums)
         dp, res = [1]*sz, nums[0]
         for i in xrange(0, sz):
@@ -8,3 +8,12 @@ class Solution(object):
                 dp[k] *= nums[k+i]
                 res = max(dp[k], res)
         return res
+    def maxProduct(self, nums):
+        r, prevMax, prevMin = nums[0], nums[0], nums[0]
+        for i in nums[1:]:
+            if i < 0:
+                prevMax, prevMin = prevMin, prevMax
+            prevMax = max(prevMax*i, i)
+            prevMin = min(prevMin*i, i)
+            r = max(r, prevMax)
+        return r
