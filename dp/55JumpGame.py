243a6769fff68e712234f7d8ca92e30cdd7721c1
lo-tp <regesteraccount@hotmail.com>
2019-02-28 09:38:28 +0800

    55 improved but still tle

--- dp/55JumpGame.py
+++ dp/55JumpGame.py
@@ -2,7 +2,7 @@ class Solution(object):
     def jumpFrom(self, index):
         if index == self.sz:
             return True
-        for i in xrange(1, self.nums[index]+1):
+        for i in xrange(1, self.nums[index]+1).reverse():
             nextIndex = i+index
             if nextIndex not in self.data:
                 if self.jumpFrom(nextIndex):
@@ -15,6 +15,21 @@ class Solution(object):
         if self.sz == -1:
             return False
         return self.jumpFrom(0)
+
+    def canJumpWithoutRecursion(self, nums):
+        lastIndex, stack = len(nums)-1, [0]
+        while stack:
+            k = stack.pop()
+            if k >= lastIndex:
+                return True
+            if nums[k] is not None:
+                for i in xrange(k+1, k+nums[k]+1):
+                    if i >= lastIndex:
+                        return True
+                    if nums[i] is not None:
+                        stack.append(i)
+                nums[k] = None
+        return False
 
 
 s = Solution()
