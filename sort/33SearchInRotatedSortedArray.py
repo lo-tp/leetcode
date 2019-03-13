f84f007a0d555c6739fbdc4c71111bb7afffe0d3
lo-tp <regesteraccount@hotmail.com>
2019-03-13 15:37:58 +0800

    33 Done with clearer solution

--- sort/33SearchInRotatedSortedArray.py
+++ sort/33SearchInRotatedSortedArray.py
@@ -2,6 +2,25 @@ from sys import maxint
 
 
 class Solution:
+    def searchClearer(self, nums, target):
+        s, e = 0, len(nums)-1
+        if e >= 0 and target == nums[e]:
+            return e
+        e -= 1
+
+        while s <= e:
+            m = s+(e-s)/2
+            if target < nums[-1] and nums[m] > nums[-1]:
+                s = m+1
+            elif target > nums[-1] and nums[m] < nums[-1]:
+                e = m-1
+            elif target < nums[m]:
+                e = m-1
+            elif target > nums[m]:
+                s = m+1
+            else:
+                return m
+        return -1
     def search(self, nums, target):
         s, e = 0, len(nums)-1
         while s <= e:
