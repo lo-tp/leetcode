b88ac9feba38134d9d63691bfd7283871482a798
lo-tp <regesteraccount@hotmail.com>
2019-03-22 08:37:21 +0800

    75 done with counting sort

--- sort/75SortColors.py
+++ sort/75SortColors.py
@@ -21,3 +21,12 @@ def qc(arr, start, end):
 class Solution(object):
     def sortColors(self, nums):
         qc(nums, 0, len(nums)-1)
+    def sortColorsWithCountingSort(self, nums):
+        data = [0, 0, 0]
+        for i in nums:
+            data[i] += 1
+        index = 0
+        for num in xrange(0, 3):
+            for w in xrange(0, data[num]):
+                nums[index] = num
+                index += 1
