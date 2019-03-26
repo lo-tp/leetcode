5fdf3fb597157ba98516093f6ac8bb453273d86c
lo-tp <regesteraccount@hotmail.com>
2019-03-26 09:06:08 +0800

    84 done with better way

--- dp/84LargestRectangleInHistogram.py
+++ dp/84LargestRectangleInHistogram.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def largestRectangleArea(self, heights):
+    def largestRectangleAreaQuicker(self, heights):
         heights.append(0)
         res, s = 0, [(0, heights[0])]
         for i in xrange(1, len(heights)):
@@ -11,4 +11,17 @@ class Solution(object):
                     w = s.pop()
                     res = max((i-w[0])*w[1], res)
                 s.append((w[0], heights[i]))
+        return res
+    def largestRectangleArea(self, heights):
+        heights.append(0)
+        res, s = 0, [(0, heights[0])]
+        for i in xrange(1, len(heights)):
+            if heights[i] < s[-1][1]:
+                w = None
+                while s and s[-1][1] > heights[i]:
+                    w = s.pop()
+                    res = max(res, (i-w[0])*w[1])
+                s.append((w[0], heights[i]))
+            else:
+                s.append((i, heights[i]))
         return res
