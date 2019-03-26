f396ac6200e00ba471c076062015cc395546df8e
lo-tp <regesteraccount@hotmail.com>
2019-03-26 09:05:43 +0800

    84 done with better way

--- dp/84LargestRectangleInHistogram.py
+++ dp/84LargestRectangleInHistogram.py
@@ -3,12 +3,12 @@ class Solution(object):
         heights.append(0)
         res, s = 0, [(0, heights[0])]
         for i in xrange(1, len(heights)):
-            if heights[i] < s[-1][1]:
-                w = None
-                while s and s[-1][1] > heights[i]:
-                    w = s.pop()
-                    res = max(res, (i-w[0])*w[1])
-                s.append((w[0], heights[i]))
-            else:
+            if heights[i] > s[-1][1]:
                 s.append((i, heights[i]))
+            elif heights[i] < s[-1][1]:
+                w = None
+                while s and heights[i] < s[-1][1]:
+                    w = s.pop()
+                    res = max((i-w[0])*w[1], res)
+                s.append((w[0], heights[i]))
         return res
