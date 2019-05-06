363a2fac821cbc6f9e4257662e3d78c696e881db
lo-tp <regesteraccount@hotmail.com>
2019-05-06 15:23:24 +0800

    10 done without recursion

--- dp/10RegularExpressionMatching.py
+++ dp/10RegularExpressionMatching.py
@@ -1,4 +1,22 @@
 class Solution(object):
+    def isMatchNonRecursive(self, str, pattern):
+        str_size, pattern_size = len(str), len(pattern)
+        dp = [[False for i in xrange(str_size+1)]
+              for k in xrange(pattern_size+1)]
+        dp[pattern_size][str_size] = True
+        for str_index in xrange(str_size, -1, -1):
+            for pattern_index in xrange(pattern_size-1, -1, -1):
+                if pattern_index == pattern_size:
+                    dp[pattern_index][str_index] = str_index == str_size
+                else:
+                    current_match = str_index < str_size and pattern[pattern_index] in {
+                        '.', str[str_index]}
+                    if pattern_index+1 < pattern_size and pattern[pattern_index+1] == '*':
+                        dp[pattern_index][str_index] = dp[pattern_index +
+                                                          2][str_index] or (current_match and dp[pattern_index][str_index+1])
+                    else:
+                        dp[pattern_index][str_index] = current_match and dp[pattern_index+1][str_index+1]
+        return dp[0][0]
     def dp(self, str_index, pattern_index):
         if (str_index, pattern_index) not in self.data:
             if pattern_index == self.pattern_size:
