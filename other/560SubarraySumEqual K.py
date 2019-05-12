82430c23a06be912670491813a921cda2245e2e7
lo-tp <regesteraccount@hotmail.com>
2019-05-12 20:20:12 +0800

    560 done

--- other/560SubarraySumEqual K.py
+++ other/560SubarraySumEqual K.py
@@ -1,5 +1,16 @@
 class Solution(object):
     def subarraySum(self, nums, k):
+        res, sum, data = 0, 0, {0: 1}
+        for i in nums:
+            sum += i
+            if sum-k in data:
+                res += data[sum-k]
+            if sum in data:
+                data[sum] += 1
+            else:
+                data[sum] = 1
+        return res
+    def subarraySumTLE(self, nums, k):
         res, size = 0, len(nums)
         for start in xrange(0, size):
             tmp = 0
