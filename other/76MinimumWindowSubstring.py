3788f3069fd9d458af7e818b90437a6953650cdc
lo-tp <regesteraccount@hotmail.com>
2019-05-16 20:19:00 +0800

    76 done better

--- other/76MinimumWindowSubstring.py
+++ other/76MinimumWindowSubstring.py
@@ -3,6 +3,31 @@ from collections import Counter
 
 
 class Solution(object):
+    def minWindowBetter(self, source, target):
+        source_size, target_size, start, end, start_index, end_index = len(
+            source), len(target), 0, 0, 0, maxint
+        if target_size and target_size <= source_size:
+            data = Counter(target)
+            remaining_num = len(data.keys())
+            while True:
+                while remaining_num and end < source_size:
+                    if source[end] in data:
+                        data[source[end]] -= 1
+                        if not data[source[end]]:
+                            remaining_num -= 1
+                    end += 1
+                if remaining_num:
+                    break
+                else:
+                    while not remaining_num and start < end:
+                        if end-start < end_index-start_index:
+                            end_index, start_index = end, start
+                        if source[start] in data:
+                            if not data[source[start]]:
+                                remaining_num += 1
+                            data[source[start]] += 1
+                        start += 1
+        return '' if maxint == end_index else source[start_index:end_index]
     def minWindow(self, str, target):
         res, start,  end, data, left_index, right_index, size = '', 0, maxint, Counter(
             target), 0, 0, len(str)
