53376ca81023d3f4664e0fba1e2f9feebb355634
lo-tp <regesteraccount@hotmail.com>
2019-05-19 15:37:02 +0800

    632 done with official solution but still TLE

--- other/632SmallestRange.py
+++ other/632SmallestRange.py
@@ -145,3 +145,56 @@ class Solution(object):
                         remaining_num += 1
                 start += 1
         return [head, tail]
+
+
+
+class MaxHeap():
+    def __init__(self, data):
+        self.data = data
+        self.size = len(data)
+
+    def sort(self):
+        if self.data > 1:
+            index = (self.size-2)/2
+            while index >= 0:
+                max_index = index*2+1
+                right_index = max_index+1
+                if right_index < self.size and self.data[right_index][0] > self.data[max_index][0]:
+                    max_index = right_index
+                if self.data[max_index][0] > self.data[index][0]:
+                    self.data[index], self.data[max_index] = self.data[max_index], self.data[index]
+                index -= 1
+
+    def get(self):
+        res = self.data[0]
+        self.size -= 1
+        if self.size >= 0:
+            self.data[0] = self.data[self.size]
+        return res
+
+    def add(self, element):
+        self.data[self.size] = element
+        self.size += 1
+
+
+class Solution(object):
+    def smallestRangeWithOfficialSolution(self, nums):
+        nums = [k for k in nums if len(k)]
+        start, end = 0, maxint
+        if len(nums):
+            end_data = [(x[-1], index, len(x)-1)
+                        for index, x in enumerate(nums)]
+            min_num = min([x[0] for x in end_data])
+            heap = MaxHeap(end_data)
+            while True:
+                heap.sort()
+                max_num, outter_index, inner_index = heap.get()
+                if max_num-min_num <= end-start:
+                    start, end = min_num, max_num
+                if not inner_index:
+                    return [start, end]
+                inner_index -= 1
+                heap.add((nums[outter_index][inner_index],
+                          outter_index, inner_index))
+                min_num = min(min_num, nums[outter_index][inner_index])
+
