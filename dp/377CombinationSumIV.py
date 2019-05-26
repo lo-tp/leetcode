8de9f6afe35ee118708f3f4f14e891eddf8a272c
lo-tp <regesteraccount@hotmail.com>
2019-05-26 18:37:00 +0800

    377 done without recursion

--- dp/377CombinationSumIV.py
+++ dp/377CombinationSumIV.py
@@ -12,3 +12,15 @@ class Solution(object):
         self.dp, self.nums = {0: 1}, nums
         return self.find(target)
 
+    def combinationSum4WithoutRecursion(self, nums, target):
+        dp = {0: 1}
+        for i in xrange(0, target):
+            if i in dp:
+                for num in nums:
+                    next = i+num
+                    if next in dp:
+                        dp[next] += dp[i]
+                    else:
+                        dp[next] = dp[i]
+
+        return dp[target] if target in dp else 0
