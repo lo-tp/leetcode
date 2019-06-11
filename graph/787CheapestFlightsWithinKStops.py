9550639ccf1345a29905baede2ecc76f08fed962
lo-tp <regesteraccount@hotmail.com>
2019-06-11 14:33:19 +0800

    787 done with dijkstra

--- graph/787CheapestFlightsWithinKStops.py
+++ graph/787CheapestFlightsWithinKStops.py
@@ -2,6 +2,20 @@ from sys import maxint
 
 
 class Solution(object):
+    def findCheapestPriceWithDijkstra(self, n, flights, src, dst, K):
+        graph, heap, res = [[]
+                            for i in xrange(0, n)], [(0, 0, src)], [maxint] * n
+        for source, dest, price in flights:
+            graph[source].append((dest, price))
+        K += 1
+        while heap:
+            price, step, node = heapq.heappop(heap)
+            if step <= K and price<=res[dst]:
+                res[node] = min(price, res[node])
+                step += 1
+                for dest, p in graph[node]:
+                    heapq.heappush(heap,(price+p, step, dest))
+        return res[dst] if res[dst] != maxint else -1
     def findCheapestPrice(self, n, flights, src, dst, K):
         graph, dp, res, stack = [[] for i in xrange(
             0, n)], [[maxint, maxint] for i in xrange(0, n)], maxint, [(src, 0, 0)]
