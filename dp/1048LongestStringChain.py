c26e387c99a8fbb894880318077168f60163edff
lo-tp <regesteraccount@hotmail.com>
2019-06-24 17:09:54 +0800

    1048 done with an better way of DP

--- dp/1048LongestStringChain.py
+++ dp/1048LongestStringChain.py
@@ -27,3 +27,29 @@ class Solution(object):
             for next_index in graph[index]:
                 stack.append((next_index, length))
         return res
+
+// dp
+def makeGraph(data):
+    size, tmp = len(data), {}
+    res = [[] for i in xrange(0, size)]
+    for index, word in enumerate(data):
+        tmp[word] = index
+        tem = list(word)
+        for i in xrange(0, len(tem)):
+            temp = tem[i]
+            tem[i] = ''
+            potential_pre = ''.join(tem)
+            if potential_pre in tmp:
+                res[tmp[potential_pre]].append(index)
+            tem[i] = temp
+    return res
+
+
+class Solution(object):
+    def longestStrChain(self, words):
+        words, size=sorted(words, key=lambda k: len(k)), len(words)
+        dp, graph = [1]*size, makeGraph(words)
+        for index in xrange(size-1,-1,-1):
+            if graph[index]:
+                dp[index]=max([ dp[next_index] for next_index in graph[index])+1
+        return max(dp)
