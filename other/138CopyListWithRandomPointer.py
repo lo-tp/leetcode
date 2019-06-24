51829f1d86f03196ce5bde9216aeb526c85777ae
lo-tp <regesteraccount@hotmail.com>
2019-06-24 09:31:52 +0800

    138 done in a better way

--- other/138CopyListWithRandomPointer.py
+++ other/138CopyListWithRandomPointer.py
@@ -1,4 +1,25 @@
 class Solution(object):
+    def copyRandomListBetter(self, head):
+        if head:
+            tem = head
+            while tem:
+                tem.next = Node(tem.val, tem.next, None)
+                tem = tem.next.next
+            tem, tmp = head, head.next
+            while tem:
+                if tem.random:
+                    tem.next.random = tem.random.next
+                tem = tem.next.next
+
+            tem = head
+            temp = head.next
+            while tem:
+                tem.next = tem.next.next
+                if tmp.next:
+                    tmp.next = tmp.next.next
+                tem, tmp = tem.next, tmp.next
+            return temp
+        return None
     def copyRandomList(self, head):
         tem, tmp = head, Node(0, None, None)
         temp = tmp
