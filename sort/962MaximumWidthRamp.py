062726d44ff0b98d7481fe8cdf69c30cee1d7d20
lo-tp <regesteraccount@hotmail.com>
2019-08-06 09:24:07 +0800

    962 done

--- sort/962MaximumWidthRamp.py
+++ sort/962MaximumWidthRamp.py
@@ -1,5 +1,15 @@
 class Solution(object):
     def maxWidthRamp(self, A):
+        indexes = sorted(
+            [index for index in xrange(0, len(A))], key=lambda i: A[i])
+        res, minimum = 0, maxint
+        for i in indexes:
+            res = max(res, i-minimum)
+            minimum = min(i, minimum)
+        return res
+
+
+    def maxWidthRampTLE(self, A):
         res, size = 0, len(A)
         for i in xrange(0, size):
             end = size-1
