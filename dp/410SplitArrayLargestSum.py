e5b7fcab525aa443b320a01aac9d9ff33fc942d8
lo-tp <regesteraccount@hotmail.com>
2019-09-30 18:00:44 +0800

    410 done with an easier to understand dp

--- dp/410SplitArrayLargestSum.py
+++ dp/410SplitArrayLargestSum.py
@@ -33,3 +33,16 @@ class Solution(object):
                                 for w in xrange(j-1, i)])
         return dp[size-1][m-1]
 
+    def splitArrayWithDPEasierToUnderstand(self, nums, m):
+        size = len(nums)
+        dp = [[0 for _ in xrange(0, m)] for _ in xrange(0, size)]
+        tmp = 0
+        for i in xrange(0, size):
+            tmp += nums[i]
+            dp[i][0] = tmp
+
+        for w in xrange(1, m):
+            for i in xrange(w, size):
+                dp[i][w] = min([max(dp[k][w-1], dp[i][0]-dp[k][0]) for k in xrange(w-1,i)])
+        return dp[size-1][m-1]
+
