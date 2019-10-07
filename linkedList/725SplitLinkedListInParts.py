bc573a2f94b599e6c6424376a8b3e7461b52348f
lo-tp <regesteraccount@hotmail.com>
2019-10-07 12:05:12 +0800

    725 done better

--- linkedList/725SplitLinkedListInParts.py
+++ linkedList/725SplitLinkedListInParts.py
@@ -1,29 +1,22 @@
 class Solution(object):
     def splitListToParts(self, root, k):
-        size, tmp = 0, root
+        tem, res, size, tmp = [1]*k, [None]*k, 0, root
         while tmp:
             size += 1
             tmp = tmp.next
-        if k >= size:
-            tmp, res = 0, [None for _ in xrange(0, k)]
-            while root:
-                res[tmp] = root
-                root = root.next
-                tmp += 1
-            for i in [r for r in res if r]:
-                r.next = None
-            return res
-        else:
+        for i in xrange(size, k):
+            tem[i] = 0
+        if k < size:
             tmp = size/k
-            tem = [tmp for i in xrange(0, k)]
+            tem = [tmp]*k
             for i in xrange(0, size % k):
                 tem[i] += 1
-            res = [ListNode() for _ in xrange(0, k)]
-            for index, i in tem:
+        for index, i in enumerate(tem):
+            if(i):
                 res[index] = root
                 for j in xrange(1, i):
                     root = root.next
                 tmp = root.next
                 root.next = None
                 root = tmp
-            return res
+        return res
