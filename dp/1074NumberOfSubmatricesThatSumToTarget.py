22983eb6d63183929c7c4aa43ffafa2531049e0e
lo-tp <regesteraccount@hotmail.com>
2019-10-15 09:15:28 +0800

    1074 done with DP

--- dp/1074NumberOfSubmatricesThatSumToTarget.py
+++ dp/1074NumberOfSubmatricesThatSumToTarget.py
@@ -3,7 +3,27 @@ def getMatrixStr(x1, y1, x2, y2):
 
 
 class Solution(object):
-    def numSubmatrixSumTarget(self, matrix, target):
+    def numSubmatrixSumTarget(self, A, target):
+        res, column_size = 0, len(A)
+        if column_size:
+            row_size = len(A[0])
+            if row_size:
+                for row in A:
+                    for i in xrange(0, row_size-1):
+                        row[i+1] += row[i]
+
+                for l in xrange(0, row_size):
+                    for r in xrange(l, row_size):
+                        data = defaultdict(lambda: 0)
+                        data[0] = 1
+                        current = 0
+                        for k in xrange(0, column_size):
+                            current += A[k][r] - \
+                                (0 if l == 0 else A[k][l-1])
+                            res += data[current-target]
+                            data[current] += 1
+        return res
+    def numSubmatrixSumTargetTLE(self, matrix, target):
         data, res, column_size = {}, 0, len(matrix)
         if column_size:
             row_size = len(matrix[0])
