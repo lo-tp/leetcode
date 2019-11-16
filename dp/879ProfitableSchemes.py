2a617c08ba0802d3bad5faefe95b2d851af9541a
lo-tp <regesteraccount@hotmail.com>
2019-11-16 22:42:24 +0800

    879 done

--- dp/879ProfitableSchemes.py
+++ dp/879ProfitableSchemes.py
@@ -3,6 +3,20 @@ from copy import deepcopy
 
 class Solution(object):
     def profitableSchemes(self, G, P, group, profit):
+        MOD, dp = 10**9+7, [[0]*(G+1) for _ in xrange(0, P+1)]
+        dp[0][0] = 1
+        for g, p in zip(group, profit):
+            tmp = [row[:] for row in dp]
+            for g1 in xrange(0, G+1-g):
+                for p1 in xrange(0, P+1):
+                    p2 = min(p1+p, P)
+                    g2 = g1+g
+                    tmp[p2][g2] += dp[p1][g1]
+                    tmp[p2][g2] %= MOD
+            dp = tmp
+        return sum(dp[-1]) % MOD
+
+    def profitableSchemes(self, G, P, group, profit):
         mod, res, size = pow(10, 9)+7, 0, len(profit)
         data = sorted([[profit[i], group[i]]
                        for i in xrange(0, size)], key=lambda k: -k[0]/k[1])
