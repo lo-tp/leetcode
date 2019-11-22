6e3ff7d15b24b637db61c75c386cf00ffd059a1d
lo-tp <regesteraccount@hotmail.com>
2019-11-22 09:22:37 +0800

    137 done

--- bitwise/137SingleNumberII.py
+++ bitwise/137SingleNumberII.py
@@ -1,5 +1,22 @@
 class Solution(object):
     def singleNumber(self, nums):
+        a, b = -0, -0
+        for num in nums:
+            for i in xrange(0, 32):
+                bit_number = 1 << i
+                if num & bit_number:
+                    if a & bit_number:
+                        a ^= bit_number
+                    elif b & bit_number:
+                        b ^= bit_number
+                        a |= bit_number
+                    else:
+                        b ^= bit_number
+        if b & (1 << 31):
+            return - (2 ** 32-b)
+        return b
+
+    def singleNumberNoNegativeSupport(self, nums):
         a, b = 0, 0
         for num in nums:
             for i in xrange(0, 32):
