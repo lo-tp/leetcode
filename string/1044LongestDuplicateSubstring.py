1d8abb16249f8f04b2d11bdcf17bb38191d47e0e
lo-tp <regesteraccount@hotmail.com>
2019-11-29 09:27:18 +0800

    1044 done but M(emory)LE

--- string/1044LongestDuplicateSubstring.py
+++ string/1044LongestDuplicateSubstring.py
@@ -16,11 +16,32 @@ class Prie():
 
 
 class Solution(object):
-    def longestDupSubstring(self, S):
+    def longestDupSubstringTLE(self, S):
         prie, res = Prie(), ''
         for i in xrange(0, len(S)):
             tmp = prie.add(S, i)
             if len(res) < tmp:
                 res = S[i:i+tmp]
+        return res
+    def longestDupSubstringMLE(self, S):
+        res, l, r, a_num = '', 0, len(S)-1, ord('a')
+        while l <= r:
+            tmp, id, existence, size = None, 0, set(), l+(r-l)/2
+            tem = 26**(size-1)
+            for i in xrange(0, size):
+                id = id*26+ord(S[i])-a_num
+            existence.add(id)
+            for i in xrange(size, len(S)):
+                tail, prev_head = S[i], S[i-size]
+                id = (id - (ord(prev_head)-a_num)*tem)*26+ord(tail)-a_num
+                if id in existence:
+                    tmp = S[i-size+1:i+1]
+                    break
+                existence.add(id)
+            if tmp:
+                l = size+1
+                res = tmp
+            else:
+                r = size-1
         return res
 
