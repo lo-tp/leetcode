1e11101bcf659df8145b08a6da3ee662daa7cde4
lo-tp <regesteraccount@hotmail.com>
2019-12-30 08:49:21 +0800

    685 done

--- graph/685RedundantConnectionII.py
+++ graph/685RedundantConnectionII.py
@@ -1,5 +1,6 @@
+from collections import defaultdict
 class Solution(object):
-    def findRedundantDirectedConnection(self, edges):
+    def findRedundantDirectedConnectionTLE(self, edges):
         appearance, num = set(), 0
         index = 1
         for input, output in edges:
@@ -12,3 +13,38 @@ class Solution(object):
             if num == index:
                 return [input, output]
             index += 1
+
+    def findRedundantDirectedConnection(self, edges):
+        special_node, N = 0, len(edges)
+        parent, child = [[]
+                         for _ in xrange(0, N+1)], [[] for _ in xrange(0, N+1)]
+        for src, dest in edges:
+            parent[dest].append(src)
+            child[src].append(dest)
+            if len(parent[dest]) == 2:
+                special_node = dest
+         # todo: when there is no node with two incoming edges
+        if not special_node:
+            graph = [[] for _ in xrange(0, N+1)]
+            for src, dest in edges:
+                graph[src].append(dest)
+                seen = set()
+                stack = graph[src][:]
+                while stack:
+                    tmp = stack.pop()
+                    if tmp not in seen:
+                        if tmp == src:
+                            return [src, dest]
+                        stack.extend(graph[tmp])
+                    seen.add(tmp)
+        src = parent[special_node][1]
+        child[src] = filter(lambda k: k != special_node, child[src])
+        appearance, stack = set(), [special_node]
+        while stack:
+            tem = stack.pop()
+            if tem in appearance:
+                return [parent[special_node][0], special_node]
+            appearance.add(tem)
+            stack.extend(child[tem])
+        return [src, special_node]
+
