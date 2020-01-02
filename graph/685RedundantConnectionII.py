268dd789a57e7bae32bc80fb0d54f613b1a495c4
lo-tp <regesteraccount@hotmail.com>
2020-01-02 13:30:58 +0800

    685 done with disjoint-set

--- graph/685RedundantConnectionII.py
+++ graph/685RedundantConnectionII.py
@@ -1,4 +1,21 @@
 from collections import defaultdict
+class DisjointSet():
+    def __init__(self, size):
+        self.rep = [i for i in xrange(0, size)]
+
+    def find(self, t):
+        stack = []
+        while t != self.rep[t]:
+            stack.append(t)
+            t = self.rep[t]
+
+        for i in stack:
+            self.rep[i] = t
+        return t
+
+    def union(self, a, b):
+        rep_a, rep_b = self.find(a), self.find(b)
+        self.rep[rep_a] = self.rep[rep_b]
 class Solution(object):
     def findRedundantDirectedConnectionTLE(self, edges):
         appearance, num = set(), 0
@@ -13,6 +30,33 @@ class Solution(object):
             if num == index:
                 return [input, output]
             index += 1
+
+    def findRedundantDirectedConnectionWithDisjointSet(self, edges):
+        special_node, parent, N = None, {}, len(edges)
+        rep, tmp, child = DisjointSet(N+1), 0, [[]
+                                                for _ in xrange(0, N+1)]
+        for src, dest in edges:
+            if dest in parent:
+                tmp, special_node = src, dest
+            else:
+                parent[dest] = src
+            child[src].append(dest)
+        if special_node:
+            child[tmp] = filter(lambda k: k != special_node, child[tmp])
+            stack = [i for i in xrange(0, N+1)]
+            while stack:
+                k = stack.pop()
+                for c in child[k]:
+                    rep_c, rep_k = rep.find(c), rep.find(k)
+                    if rep_c == rep_k:
+                        return [parent[special_node], special_node]
+                    rep.union(rep_c, rep_k)
+            return [tmp, special_node]
+        for src, dest in edges:
+            rep_a, rep_b = rep.find(src), rep.find(dest)
+            if rep_a == rep_b:
+                return [src, dest]
+            rep.union(rep_a, rep_b)
 
     def findRedundantDirectedConnection(self, edges):
         special_node, N = 0, len(edges)
