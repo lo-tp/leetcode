94abf8f8846a4e2b83db7922c2fbab735cbd8210
lo-tp <regesteraccount@hotmail.com>
2020-03-05 20:53:22 +0800

    775 done

--- other/775GlobalAndLocalInversions.py
+++ other/775GlobalAndLocalInversions.py
@@ -1,5 +1,5 @@
 class Solution(object):    
-    def isIdealPermutation(self, A):
+    def isIdealPermutationButTLE(self, A):
         sz = len(A)
         for i in xrange(0, sz-2):
             for j in xrange(i+2, sz):
@@ -7,3 +7,10 @@ class Solution(object):
                     return False
         return True
 
+    def isIdealPermutation(self, A):
+        minimum, sz = A[-1], len(A)
+        for i in xrange(sz-3, -1, -1):
+            if A[i] > minimum:
+                return False
+            minimum = min(minimum, A[i+1])
+        return True
