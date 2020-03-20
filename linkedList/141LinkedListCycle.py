0b5123f73ab95b97dfb4f2e8d8adf71a4eeb6917
lo-tp <regesteraccount@hotmail.com>
2020-03-20 09:43:40 +0800

    141 done better

--- linkedList/141LinkedListCycle.py
+++ linkedList/141LinkedListCycle.py
@@ -1,21 +1,39 @@
 class Solution(object):
     def hasCycle(self, head):
+        f = head.next if head else head
+        while f and f.next:
+            if f == head:
+                return True
+            head = head.next
+            f = f.next.next
+        return False
+
+   def hasCycle(self, head):
+        seen = set()
+        while head:
+            if head in seen:
+                return True
+            seen.add(head)
+            head = head.next
+        return False
+
+    def hasCycle(self, head):
         """
         :type head: ListNode
         :rtype: bool
         """
-        quick=head
-        slow=head
+        quick = head
+        slow = head
         while head:
-            quick=quick.next
+            quick = quick.next
             if not quick:
                 return False
-            if quick==slow:
+            if quick ==slow:
                 return True
-            quick=quick.next
+            quick = quick.next
             if not quick:
                 return False
-            if quick==slow:
+            if quick ==slow:
                 return True
-            slow=slow.next
+            slow = slow.next
         return False
