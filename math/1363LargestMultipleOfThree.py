5a3a79e9007a148532b616da75c1260957b6d4ee
lo-tp <regesteraccount@hotmail.com>
2020-03-30 20:57:05 +0800

    1363 done better

--- math/1363LargestMultipleOfThree.py
+++ math/1363LargestMultipleOfThree.py
@@ -57,3 +57,41 @@ class Solution(object):
             res = sub('^0+', '0', res)
         return res
 
+    def largestMultipleOfThree(self, digits):
+        res, residue, data = '', sum(digits) % 3, Counter(digits)
+        if residue == 2:
+            for i in [i for i in [2, 5, 8] if i in data]:
+                data[i] -= 1
+                residue -= 2
+                break
+            if residue:
+                for i in [i for i in [1, 4, 7] if i in data]:
+                    if residue:
+                        residue -= 1
+                        data[i] -= 1
+                    if residue and data[i]:
+                        residue -= 1
+                        data[i] -= 1
+                    if not residue:
+                        break
+        elif residue:
+            for i in [i for i in [1, 4, 7] if i in data]:
+                data[i] -= 1
+                residue -= 1
+                break
+            if residue:
+                residue = 2
+                for i in [i for i in [2, 5, 8] if i in data]:
+                    if residue:
+                        residue -= 1
+                        data[i] -= 1
+                    if residue and data[i]:
+                        residue -= 1
+                        data[i] -= 1
+                    if not residue:
+                        break
+        if not residue:
+            for i in [i for i in xrange(9, -1, -1) if i in data and data[i]]:
+                res += str(i)*data[i]
+        return sub('^0+', '0', res)
+
