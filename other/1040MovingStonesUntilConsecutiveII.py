0bb4a3687cd2026d85096382f1076d42c6ac41ca
lo-tp <regesteraccount@hotmail.com>
2020-03-31 08:06:12 +0800

    1040 done

--- other/1040MovingStonesUntilConsecutiveII.py
+++ other/1040MovingStonesUntilConsecutiveII.py
@@ -3,6 +3,18 @@ from sys import maxint
 
 
 class Solution(object):
+    def numMovesStonesII(self, s):
+        sz, l, r = len(s), 0, 0
+        s.sort()
+        minimum, maximum = sz, max(s[-1]-s[1]-sz+2, s[-2]-s[0]-sz+2)
+        for r in xrange(0, sz):
+            while s[r]-s[l]+1 > sz:
+                l += 1
+            if r-l+1 == sz-1 and s[r] == s[l]+sz-2:
+                minimum = min(2, minimum)
+            else:
+                minimum = min(minimum, sz-r+l-1)
+        return [minimum, maximum]
     def numMovesStonesII(self, stones):
         stones.sort()
         sz, low = len(stones), maxint
