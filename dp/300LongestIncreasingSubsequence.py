b37b24fa449974e224f8d8904b13139011a325de
lo-tp <regesteraccount@hotmail.com>
2020-04-01 09:51:46 +0800

    300 done better

--- dp/300LongestIncreasingSubsequence.py
+++ dp/300LongestIncreasingSubsequence.py
@@ -1,3 +1,5 @@
+from sys import maxint
+from bisect import bisect_left
 class Solution(object):
     def lengthOfLIS(self, nums):
         dp = [1 for i in nums]
@@ -37,3 +39,14 @@ class Solution(object):
                             records[m] = i
                             break
         return ret
+
+    def lengthOfLISBetter(self, nums):
+        dp = [-maxint]
+        for i in nums:
+            if i > dp[-1]:
+                dp.append(i)
+            else:
+                j = bisect_left(dp, i)
+                dp[j] = i
+        return len(dp)-1
+
