b8b82f1cd4e9194fb3a24a8b2592907a22929111
lo-tp <regesteraccount@hotmail.com>
2020-04-10 12:45:14 -0600

    992 done in different way

--- other/992SubarraysWithKDifferentIntegers.py
+++ other/992SubarraysWithKDifferentIntegers.py
@@ -3,6 +3,27 @@ from collections import defaultdict
 
 class Solution(object):
     def subarraysWithKDistinct(self, A, K):
+        res, l, t, data = 0, 0, 0, defaultdict(lambda: 0)
+        for r in xrange(0, len(A)):
+            if not data[A[r]]:
+                t += 1
+            data[A[r]] += 1
+            if t > K:
+                while data[A[l]] != 1:
+                    data[A[l]] -= 1
+                    l += 1
+                data[A[l]] -= 1
+                l += 1
+                t -= 1
+            if t == K:
+                tmp = defaultdict(lambda: 0)
+                for j in xrange(l, r+1):
+                    res += 1
+                    tmp[A[j]] += 1
+                    if tmp[A[j]] == data[A[j]]:
+                        break
+        return res
+    def subarraysWithKDistinct(self, A, K):
         sz = len(A)
         data = [0, 0]
         for i, j in enumerate([K-1, K]):
