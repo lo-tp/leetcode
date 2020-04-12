0d1724b072a26899d7e823357baa01df2740b742
lo-tp <regesteraccount@hotmail.com>
2020-04-12 11:30:38 -0600

    898 done

--- dp/898BitwiseORsOfSubarrays.py
+++ dp/898BitwiseORsOfSubarrays.py
@@ -15,3 +15,19 @@ class Solution(object):
                         tmp.add((tem, j))
             data = tmp
         return len(seen)
+    def subarrayBitwiseORs(self, A):
+        te, t, seen, sz = set(), 0, set(),  len(A)
+        data = [(0, i) for i in xrange(0, sz)]
+        while data:
+            tmp = []
+            for i, j in data:
+                id = '{} {}'.format(i, j)
+                if id not in te:
+                    te.add(id)
+                    tem = i | A[j]
+                    seen.add(tem)
+                    j += 1
+                    if j < sz:
+                        tmp.append((tem, j))
+            data = tmp
+        return len(seen)
