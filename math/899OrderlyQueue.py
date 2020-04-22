cd035dd0773c832548ecbcac593cdf5ad5e6c91c
lo-tp <regesteraccount@hotmail.com>
2020-04-21 21:37:09 -0600

    899 done

---
+++ math/899OrderlyQueue.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def orderlyQueueTLE(self, S, K):
+        seen, stack, res = set(), [S], S
+        while stack:
+            s = stack.pop()
+            if s in seen:
+                continue
+            seen.add(s)
+            if s < res:
+                res = s
+            for i in xrange(0, K):
+                t, te = s[:i], s[i+1:]
+                stack.append('{}{}{}'.format(t, te, s[i]))
+        return res
+
+    def orderlyQueue(self, S, K):
+        seen, stack, res = set(), [S], S
+        while stack:
+            s = stack.pop()
+            if s in seen:
+                continue
+            seen.add(s)
+            if s < res:
+                res = s
+            for i in xrange(0, K):
+                t, te = s[:i], s[i+1:]
+                stack.append('{}{}{}'.format(t, te, s[i]))
+        return res
+
