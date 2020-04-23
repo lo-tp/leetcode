ac46f1b479680f5827fd8caa72a9cafe671535ec
lo-tp <regesteraccount@hotmail.com>
2020-04-23 07:30:20 -0600

    134 done better

--- other/134GasStation.py
+++ other/134GasStation.py
@@ -1,6 +1,18 @@
 class Solution(object):
     def canCompleteCircuit(self, gas, cost):
         sz = len(gas)
+        t, l, r = gas[sz-1]-cost[sz-1], sz-1, 0
+        while l > r:
+            if t >= 0:
+                t += gas[r]-cost[r]
+                r += 1
+            else:
+                l -= 1
+                t += gas[l]-cost[l]
+        return l if t >= 0 else -1
+
+    def canCompleteCircuit(self, gas, cost):
+        sz = len(gas)
         for i in xrange(0, sz):
             te, t = 0, 0
             while te != sz:
