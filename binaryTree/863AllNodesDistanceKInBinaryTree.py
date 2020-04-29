c990dd32cdc470606da3664e610a89d86f57731c
lo-tp <regesteraccount@hotmail.com>
2020-04-28 23:50:10 -0600

    863 done better

--- binaryTree/863AllNodesDistanceKInBinaryTree.py
+++ binaryTree/863AllNodesDistanceKInBinaryTree.py
@@ -32,3 +32,30 @@ class Solution(object):
             depth += 1
             stack = t
         return []
+    def distanceKBetter(self, root, target, K):
+        stack, root.parent = [], None
+        while stack or root:
+            if root:
+                if root.left:
+                    root.left.parent = root
+                if root.right:
+                    root.right.parent = root
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop().right
+        seen, stack = set(), [target]
+        while stack or K:
+            tmp = []
+            for i in stack:
+                seen.add(i.val)
+                if i.left and i.left.val not in seen:
+                    tmp.append(i.left)
+                if i.right and i.right.val not in seen:
+                    tmp.append(i.right)
+                if i.parent and i.parent.val not in seen:
+                    tmp.append(i.parent)
+            K -= 1
+            stack = tmp
+        return stack
+
