a0fa9236e050bdb1ff0c3187246300cc695bb58b
lo-tp <regesteraccount@hotmail.com>
2020-05-07 07:12:38 -0600

    239 done with  heap

--- other/239SlidingWindowMaximum.py
+++ other/239SlidingWindowMaximum.py
@@ -1,5 +1,6 @@
 from sys import maxint
 from collections import deque
+from heapq import heapify, heappop, heappush
 
 
 class Mq:
@@ -44,4 +45,18 @@ class Solution(object):
                 mq.push(i)
                 res.append(mq.getMax())
                 mq.pop()
+        return res
+    def maxSlidingWindowWithHeap(self, nums, k):
+        res, heap = [], []
+        for i in xrange(0, k):
+            heap.append((-nums[i], i))
+        heapify(heap)
+        j = 0
+        for i in xrange(k, len(nums)):
+            res.append(-heap[0][0])
+            heappush(heap, (-nums[i], i))
+            while heap[0][1] <= j:
+                heappop(heap)
+            j += 1
+        res.append(-heap[0][0])
         return res
