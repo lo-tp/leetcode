dfe977ad9701df69841b51eae02f380c2afbbca2
lo-tp <regesteraccount@hotmail.com>
2020-05-19 21:30:01 -0600

    1358 done

--- string/1358NumberofSubstringsContainingAllThreeCharacters.py
+++ string/1358NumberofSubstringsContainingAllThreeCharacters.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def numberOfSubstrings(self, s):
+    def numberOfSubstringsTLE(self, s):
         res, t, te, data = 0, 0, {
             'a': 0, 'b': 1, 'c': 2}, [0, 0, 0]
         for r in s:
@@ -15,3 +15,22 @@ class Solution(object):
                     res += 1
                 res += 1
         return res
+    def numberOfSubstrings(self, s):
+        l, prev, res, t, te, data = 0, 0, 0, 0, {
+            'a': 0, 'b': 1, 'c': 2}, [0, 0, 0]
+        for r in s:
+            if not data[te[r]]:
+                t += 1
+            data[te[r]] += 1
+            if t == 3:
+                while data[te[s[l]]] != 1:
+                    data[te[s[l]]] -= 1
+                    l += 1
+                    prev += 1
+                data[te[s[l]]] -= 1
+                l += 1
+                t -= 1
+                prev += 1
+            res += prev
+        return res
+
