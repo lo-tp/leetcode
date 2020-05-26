32ec6d46cdc4b4b8d67c443c2422e068797a79b2
lo-tp <regesteraccount@hotmail.com>
2020-05-26 08:32:23 -0600

    233 done better

--- dp/233NumberofDigitOne.py
+++ dp/233NumberofDigitOne.py
@@ -26,3 +26,19 @@ class Solution(object):
             else:
                 res += 1
         return res
+
+    def countDigitOne(self, n):
+        if n <= 0:
+            return 0
+        ord_0, res, t = ord('0'), 0, [
+            (i+1)*10**i for i in xrange(0, int(log10(n)))]
+        while n:
+            te, tem = list(str(n)), int(log10(n))-1
+            tmp, te[0] = ord(te[0])-ord_0, '0'
+            n = int(''.join(te))
+            if tem >= 0:
+                res += tmp*t[tem]
+                res += n+1 if tmp == 1 else 10**(tem+1)
+            else:
+                res += 1
+        return res
