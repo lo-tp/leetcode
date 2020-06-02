00653c15a783f06857179cb2bd29b3a9bad10417
lo-tp <regesteraccount@hotmail.com>
2020-06-01 19:04:50 -0600

    5 done better

--- string/5LongestPalindromicSubstring.py
+++ string/5LongestPalindromicSubstring.py
@@ -1,3 +1,5 @@
+from re import sub
+
 class Solution(object):
     def longestPalindrome(self, s):
         str = '#'+'#'.join(list(s))+'#'
@@ -32,3 +34,21 @@ class Solution(object):
             if dp[i]>dp[ret]:
                 ret=i
         return str[ret-dp[ret]: ret+dp[ret]+1].replace('#','')
+    def longestPalindrome(self, s):
+        maximum, res, t = 0, 0, '-{}-'.format('-'.join(list(s)))
+        sz, l, m, r = len(t), 0, 0, 0
+        data = [0]*sz
+        for i in xrange(0, sz):
+            mirror_i = 2*m-i
+            if mirror_i < m and mirror_i >= l:
+                data[i] = min(data[mirror_i], r-i)
+            l_edge, r_edge = i-data[i]-1, i+data[i]+1
+            while l_edge > -1 and r_edge < sz and t[l_edge] == t[r_edge]:
+                l_edge -= 1
+                r_edge += 1
+                data[i] += 1
+            if r_edge-1 > r:
+                l, m, r = l_edge+1, i, r_edge-1
+            if data[i] > maximum:
+                maximum, res = data[i], i
+        return sub('-', '', t[res-data[res]: res+data[res]+1])
