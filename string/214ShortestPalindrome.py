1779416ff3334fd870bc096e2770305ca192bfa2
lo-tp <regesteraccount@hotmail.com>
2020-06-04 18:59:11 -0600

    214 done but WA

--- string/214ShortestPalindrome.py
+++ string/214ShortestPalindrome.py
@@ -1,3 +1,12 @@
+def getTable(string):
+    k, sz, table = -1, len(string), [-1 for _ in string]
+    for i in xrange(1, sz):
+        while k != -1 and string[k+1] != string[i]:
+            k = table[k]
+        if string[k+1] == string[i]:
+            k += 1
+        table[i] = k
+    return table
 class Solution(object):
     def shortestPalindrome(self, s):
         sz = len(s)
@@ -22,3 +31,14 @@ class Solution(object):
 
             return res
         return s
+    def shortestPalindromeWA(self, S):
+        maximum, rev, table, sz, k = 0, S[::-1], getTable(S), len(S), -1
+        print table
+        for i in xrange(0, sz):
+            while k != -1 and S[k+1] != rev[i]:
+                # print k, i
+                k = table[k]
+            if S[k+1] == rev[i]:
+                k += 1
+            maximum = max(maximum, k)
+        return '{}{}'.format(S[maximum+1:][::-1], S)
