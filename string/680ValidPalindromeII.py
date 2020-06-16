fd2f04662b7b943bc78b04cad0f1cdf846903983
lo-tp <regesteraccount@hotmail.com>
2020-06-16 07:33:25 -0600

    680 done better

--- string/680ValidPalindromeII.py
+++ string/680ValidPalindromeII.py
@@ -11,3 +11,21 @@ class Solution(object):
                 stack.append((l+1, r, 0))
                 stack.append((l, r-1, 0))
         return False
+    def validPalindrome(self, s):
+        seen = set()
+        stack = [(0, len(s)-1, 1)]
+        while stack:
+            l, r, t = stack.pop()
+            flag = '{},{},{}'.format(l, r, t)
+            if flag in seen:
+                continue
+            seen.add(flag)
+            if l > r:
+                return True
+            if s[l] == s[r]:
+                stack.append((l+1, r-1, t))
+            if t:
+                stack.append((l+1, r, 0))
+                stack.append((l, r-1, 0))
+        return False
+
