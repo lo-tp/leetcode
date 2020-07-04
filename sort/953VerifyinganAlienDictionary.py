7278809bb3c64f5463c11249f671b2dd49d10ec1
lo-tp <regesteraccount@hotmail.com>
2020-07-04 16:07:43 -0600

    953 done with for else clause

--- sort/953VerifyinganAlienDictionary.py
+++ sort/953VerifyinganAlienDictionary.py
@@ -14,3 +14,18 @@ class Solution(object):
             if not flag and len(t) > len(te):
                 return False
         return True
+    def isAlienSorted(self, words, order):
+        data = {}
+        for i, j in enumerate(order):
+            data[j] = i
+        for i in xrange(0, len(words)-1):
+            t, te = words[i], words[i+1]
+            for tem in xrange(0, min(len(t), len(te))):
+                if data[t[tem]] > data[te[tem]]:
+                    return False
+                elif data[t[tem]] < data[te[tem]]:
+                    break
+            else:
+                if len(t) > len(te):
+                    return False
+        return True
