3a9a6ee231d31c390d40df0d9dd2701496658d0f
lo-tp <regesteraccount@hotmail.com>
2020-07-06 16:07:43 -0600

    443 done without adding extra element

--- string/443StringCompression.py
+++ string/443StringCompression.py
@@ -14,3 +14,19 @@ class Solution(object):
             else:
                 count += 1
         return index-1
+    def compress(self, chars):
+        sz, index, count, char = len(chars)-1, 1, 0, chars[0]
+        for i, c in enumerate(chars):
+            if c == char:
+                count += 1
+            if i == sz or c != char:
+                if count > 1:
+                    for j in '{}'.format(count):
+                        chars[index] = j
+                        index += 1
+                if c != char:
+                    char = chars[index] = c
+                    index += 1
+                    count = 1
+        return index
+
