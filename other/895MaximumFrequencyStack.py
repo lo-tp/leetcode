3f8396ad324ce2c35f1e5eb65613e04e2597596f
lo-tp <regesteraccount@hotmail.com>
2020-07-09 16:07:43 -0600

    895 done but  WA

---
+++ other/895MaximumFrequencyStack.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+
+class FreqStack():
+    def __init__(self):
+        self.max, self.freq, self.data = 0, defaultdict(lambda: 0), []
+
+    def push(self, t):
+        self.freq[t] += 1
+        self.max = max(self.freq[t], self.max)
+        if self.max < len(self.data):
+            self.data.append([])
+        self.data[self.freq[t]].append(t)
+
+    def pop(self):
+        res = self.data[self.max].pop()
+        self.freq[res] -= 1
+        if not self.data[self.max]:
+            self.max -= 1
+        return res
+
