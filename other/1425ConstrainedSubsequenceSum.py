b96b6b800e22fe1ed7f1e1f69ad8a0285094ec24
lo-tp <regesteraccount@hotmail.com>
2020-07-11 16:07:43 -0600

    1425 done

--- other/1425ConstrainedSubsequenceSum.py
+++ other/1425ConstrainedSubsequenceSum.py
@@ -13,3 +13,14 @@ class Solution(object):
             heappush(data, (-t, r))
             l += 1
         return res
+    def constrainedSubsetSum(self, nums, k):
+        res, l = nums[0], -(k+1)
+        data = [(0, l+1)]
+        for r in xrange(0, len(nums)):
+            while data[0][1] <= l:
+                heappop(data)
+            t = nums[r]-min(0, data[0][0])
+            res = max(t, res)
+            heappush(data, (-t, r))
+            l += 1
+        return res
