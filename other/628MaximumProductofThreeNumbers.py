d5ea7c47a9602d3ce5079d2040229a5022430e66
lo-tp <regesteraccount@hotmail.com>
2020-07-14 16:07:43 -0600

    628 done better

--- other/628MaximumProductofThreeNumbers.py
+++ other/628MaximumProductofThreeNumbers.py
@@ -17,8 +17,27 @@ def qc(arr, start, end):
 
 class Solution(object):
     def maximumProduct(self, nums):
-        qc(nums, 0, len(nums)-1)
-        if nums[-1] <= 0 or nums[0] >= 0:
-            return nums[-1]*nums[-2]*nums[-3]
-        return nums[-1]*max(nums[0]*nums[1], nums[-2]*nums[-3])
+        min_q,max_q=nums[:2],nums[:3]
+        sort(min_q, 0, 1)
+        sort(max_q, 0, 2)
+        for i in xrange(2, len(nums)):
+            if nums[i]<min_q[-1]:
+                min_q[-1]=nums[i]
+                sort(min_q, 0, 1)
+            if nums[i]>max_q[-1]:
+                max_q[-1]=nums[i]
+                sort(max_q, 0,2)
+        return max_q[-1]*max(min_q[0]*min_q[1], max_q[0]*max_q[1])
 
+    def maximumProduct(self, nums):
+        min_q, max_q = nums[:3], nums[:3]
+        qc(min_q, 0, 2)
+        qc(max_q, 0, 2)
+        for i in xrange(3, len(nums)):
+            if nums[i] < min_q[-1]:
+                min_q[-1] = nums[i]
+                qc(min_q, 0, 2)
+            if nums[i] > max_q[0]:
+                max_q[0] = nums[i]
+                qc(max_q, 0, 2)
+        return max_q[-1]*max(min_q[0]*min_q[1], max_q[0]*max_q[1])
