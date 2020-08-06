2e5d0954d5790ac63147e7ceac2a7b4529a0dd8d
lo-tp <regesteraccount@hotmail.com>
2020-08-06 09:18:25 +0800

    1425 done with deque

--- other/1425ConstrainedSubsequenceSum.py
+++ other/1425ConstrainedSubsequenceSum.py
@@ -1,4 +1,5 @@
 from heapq import heappop, heappush
+from collections import deque
 
 
 class Solution(object):
@@ -24,3 +25,17 @@ class Solution(object):
             heappush(data, (-t, r))
             l += 1
         return res
+    def constrainedSubsetSumWithDeque(self, nums, k):
+        res, l = nums[0], -(k+1)
+        data = deque([(0, l+1)])
+        for r in xrange(0, len(nums)):
+            while data[0][1] <= l:
+                data.popleft()
+            t = nums[r]+max(0, data[0][0])
+            res = max(t, res)
+            while data and data[-1][0] < t:
+                data.pop()
+            data.append((t, r))
+            l += 1
+        return res
+
