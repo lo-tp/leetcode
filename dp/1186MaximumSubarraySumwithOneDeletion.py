d8034a83e520aa14e07299f97cf319ceb84625e6
lo-tp <regesteraccount@hotmail.com>
2020-08-07 09:02:48 +0800

    1186 done with dp

--- dp/1186MaximumSubarraySumwithOneDeletion.py
+++ dp/1186MaximumSubarraySumwithOneDeletion.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def maximumSum(self, arr):
+    def maximumSumTLE(self, arr):
         res, sums, minimums, sz = arr[0], [], [], 0
         for i in arr:
             for j in xrange(0, sz):
@@ -11,5 +11,12 @@ class Solution(object):
                 res = max(res, sums[j], sums[j]-minimums[j])
             res = max(res, sums[sz])
             sz += 1
+        return res
+    def maximumSum(self, arr):
+        res, sub, non_sub = arr[0], 0, arr[0]
+        for i in xrange(1, len(arr)):
+            sub = max(non_sub, sub+arr[i])
+            non_sub = max(0, non_sub)+arr[i]
+            res = max(res, sub, non_sub)
         return res
 
