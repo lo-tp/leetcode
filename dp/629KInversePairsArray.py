9b352254032a2c39032233c84413f1a88260ba06
lo-tp <regesteraccount@hotmail.com>
2020-08-10 08:50:38 +0800

    629 done but TLE

--- dp/629KInversePairsArray.py
+++ dp/629KInversePairsArray.py
@@ -12,3 +12,12 @@ class Solution(object):
                 for w in xrange(0, min(i-1, j)+1):
                     dp[i][j] += dp[i-1][w]
         return dp[n-1][k] % (10 ** 9+7)
+    def kInversePairs(self, n, k):
+        dp = [[0 for _ in xrange(0, k+1)] for _ in xrange(0, n)]
+        dp[0][0] = 1
+        for i in xrange(1, n):
+            dp[i][0] = 1
+            for j in xrange(1, k+1):
+                for m in xrange(0, min(i+1, j+1)):
+                    dp[i][j] += dp[i-1][j-m]
+        return dp[n-1][k]
