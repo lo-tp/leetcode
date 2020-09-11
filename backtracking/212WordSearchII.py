b406d501677ebd0ed69db55729ad4e4b90ca6275
lo-tp <regesteraccount@hotmail.com>
2020-09-11 14:15:54 +0800

    212 done in a way to destroy the sub tree in the trie to reduce search tree

--- backtracking/212WordSearchII.py
+++ backtracking/212WordSearchII.py
@@ -66,3 +66,34 @@ class Solution(object):
                                              (v+1, h), (v-1, h), (v, h+1), (v, h-1)] if w >= 0 and w < v_sz and m >= 0 and m < h_sz and board[w][m] in node])
                                 board[v][h] = '*'
         return res
+    def findWords(self, b, words):
+        v_sz, res = len(b), []
+        if v_sz:
+            h_sz = len(b[0])
+            if h_sz:
+                trie, seen, stack = {}, set(), []
+                for word in words:
+                    t = trie
+                    for w in word:
+                        t = t.setdefault(w, {})
+                    t['-'] = word
+                for v in xrange(0, v_sz):
+                    for h in xrange(0, h_sz):
+                        stack.append((v, h, trie, False))
+                while stack:
+                    v, h, p, flag = stack.pop()
+                    if b[v][h] in p:
+                        t = p[b[v][h]]
+                        if flag:
+                            seen.remove((v, h))
+                            if not t:
+                                p.pop(b[v][h])
+                        else:
+                            m = t.pop('-', False)
+                            if m:
+                                res.append(m)
+                            seen.add((v, h))
+                            stack.append((v, h, p, True))
+                            stack.extend([(i, j, t, False) for i, j in [(v+1, h), (v-1, h), (v, h+1),
+                                                                        (v, h-1)] if i >= 0 and i < v_sz and j >= 0 and j < h_sz and (i, j) not in seen])
+        return res
