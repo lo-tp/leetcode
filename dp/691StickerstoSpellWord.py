8d0ccbde88cea19aaf27983baf648ead96a34903
lo-tp <regesteraccount@hotmail.com>
2020-09-18 14:46:16 +0800

    691 done

--- dp/691StickerstoSpellWord.py
+++ dp/691StickerstoSpellWord.py
@@ -23,3 +23,26 @@ class Solution(object):
                     t_count[key] -= min(t_count[key], val)
         return -1 if sz else res
 
+    def minStickers(self, stickers, target):
+        t_count, sz, data = Counter(target), len(
+            target), [Counter(i) for i in stickers]
+        for i in xrange(0, len(data)):
+            for j in xrange(0, len(data)):
+                if i != j and data[i] & data[j] == data[j]:
+                    data[j] = Counter()
+        data = [i & t_count for i in data]
+        dp, data, t = [-1]*(2**sz), [''.join(i.elements()) for i in data if i
+                                     ], [2**i for i in xrange(0, sz)]
+        dp[0] = 0
+        for state in xrange(0, 2**sz):
+            if dp[state] != -1:
+                for i in data:
+                    now = state
+                    for letter in i:
+                        for index, c in enumerate(target):
+                            if c == letter and (not (now & t[index])):
+                                now |= t[index]
+                                break
+                    if dp[now] == -1 or dp[now] > dp[state]+1:
+                        dp[now] = dp[state]+1
+        return dp[-1]
