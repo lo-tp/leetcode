2d23617d020cf4c2ecf1febd6117be5697bacdcd
lo-tp <regesteraccount@hotmail.com>
2020-09-22 08:56:32 +0800

    372 done

--- math/372SuperPow.py
+++ math/372SuperPow.py
@@ -5,3 +5,12 @@ class Solution(object):
             t *= 10
             t += i
         return (a**t) % 1337
+    def superPow(self, a, b):
+        if not a:
+            return 0
+        mod = 1337
+        a, res = a % mod, 1
+        while b:
+            res = (res*(a**b.pop()) % mod)%mod
+            a = (a**10) % mod
+        return res
