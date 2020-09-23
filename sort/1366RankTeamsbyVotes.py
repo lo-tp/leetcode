572a5707de8fb94a56620e12c8f01c9fd44c77cb
lo-tp <regesteraccount@hotmail.com>
2020-09-23 13:40:32 +0800

    1366 done with cleaner code

--- sort/1366RankTeamsbyVotes.py
+++ sort/1366RankTeamsbyVotes.py
@@ -54,3 +54,42 @@ class Solution(object):
                 qc(arr, start, e)
         qc(res, 0, sz-1)
         return ''.join(res[::-1])
+    def rankTeams(self, votes):
+        ord_A, scores, res = ord('A'), [[0 for _ in xrange(
+            0, 26)] for _ in votes[0]], list(votes[0])
+        sz = len(votes[0])
+        mapping = {}
+        for i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            mapping[i] = ord(i)-ord_A
+
+        for vote in votes:
+            for index, letter in enumerate(vote):
+                scores[index][mapping[letter]] += 1
+        def compare(a,b):
+            if a==b:
+                return 0
+            a_index, b_index=mapping[a], mapping[b]
+            for score in scores:
+                if score[a_index]>score[b_index]:
+                    return -1
+                elif score[a_index]<score[b_index]:
+                    return 1
+            return -1 if a_index<b_index else 1
+
+        def qc(arr, start, end):
+            s, e, m = start, end, arr[start+(end-start)/2]
+            while s <= e:
+                while compare(arr[s],m)==-1:
+                    s+=1
+                while compare(arr[e],m)==1:
+                    e-=1
+                if s <= e:
+                    arr[s], arr[e] = arr[e], arr[s]
+                    s += 1
+                    e -= 1
+            if s < end:
+                qc(arr, s, end)
+            if start < e:
+                qc(arr, start, e)
+        qc(res, 0, sz-1)
+        return ''.join(res)
