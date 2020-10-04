0761b247515b455f0726c5deb6f0cb31b7d91eb2
lo-tp <regesteraccount@hotmail.com>
2020-10-04 09:48:17 +0800

    380 done with simpler code

--- other/380InsertDeleteGetRandomO(1).py
+++ other/380InsertDeleteGetRandomO(1).py
@@ -25,3 +25,28 @@ class RandomizedSet(object):
 
     def getRandom(self):
         return choice(self.data)
+
+class RandomizedSet(object):
+
+    def __init__(self):
+        self.data, self.match = [], {}
+
+    def insert(self, val):
+        if val not in self.match:
+            self.match[val] = len(self.data)
+            self.data.append(val)
+            return True
+        return False
+
+    def remove(self, val):
+        if val in self.match:
+            self.data[self.match[val]], self.match[self.data[-1]
+                                                   ] = self.data[-1], self.match[val]
+            del self.match[val]
+            self.data.pop()
+            return True
+        return False
+
+    def getRandom(self):
+        return choice(self.data)
+
