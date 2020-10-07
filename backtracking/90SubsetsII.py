8345235a2f5b5ec6b44559f71175abc2a7e8fa97
lo-tp <regesteraccount@hotmail.com>
2020-10-07 16:49:46 +0800

    90 Done with Better Way

--- backtracking/90SubsetsII.py
+++ backtracking/90SubsetsII.py
@@ -20,3 +20,21 @@ class Solution(object):
                     stack.extend([(i, False)
                                   for i in xrange(0, sz) if i not in seen_index])
         return res
+
+    def subsetsWithDup(self, nums):
+        nums.sort()
+        sz = len(nums)
+        stack, res, current = [(i, False) for i in range(
+            0, sz) if not i or nums[i] != nums[i-1]], [[]], []
+        while stack:
+            index, visited = stack.pop()
+            if visited:
+                res.append(current[:])
+                current.pop()
+            else:
+                stack.append((index, True))
+                current.append(nums[index])
+                stack.extend([(i, False) for i in range(index+1, sz)
+                              if i == index+1 or nums[i] != nums[i-1]])
+        return res
+
