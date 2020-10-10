2945fbf5f0e5a78f9b986bb7edda441abe2a03e4
lo-tp <regesteraccount@hotmail.com>
2020-10-10 20:53:13 +0800

    791 done with non-recursive quick sort

--- sort/791CustomSortString.py
+++ sort/791CustomSortString.py
@@ -25,3 +25,27 @@ class Solution(object):
         t = list(T)
         self.qc(t, 0, len(t)-1)
         return ''.join(t)
+def customSortString(self, S, T):
+        self.weight = defaultdict(lambda: 0)
+        for index, s in enumerate(S):
+            self.weight[s] = index
+        string = list(T)
+        stack=[(0, len(string)-1)]
+        while stack:
+            start, end=stack.pop()
+            s, e, m = start, end, string[start+(end-start)/2]
+            while s <= e:
+                while self.weight[string[s]] < self.weight[m]:
+                    s += 1
+                while self.weight[string[e]] > self.weight[m]:
+                    e -= 1
+                if s <= e:
+                    string[s],  string[e] = string[e], string[s]
+                    s += 1
+                    e -= 1
+            if s < end:
+                stack.append((s, end))
+            if start < e:
+                stack.append((start, e))
+        return ''.join(string)
+
