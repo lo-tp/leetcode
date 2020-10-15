c2f328fcc1e93617a4e4ffbc67b2793d5662d0f6
lo-tp <regesteraccount@hotmail.com>
2020-10-15 22:23:53 +0800

    1542 done without using huge array

--- dp/1542FindLongestAwesomeSubstring.py
+++ dp/1542FindLongestAwesomeSubstring.py
@@ -15,3 +15,22 @@ class Solution(object):
                     if data[tmp] != -2:
                         res = max(res, index-data[tmp])
         return res
+    def longestAwesome(self, s):
+        ord_0, data, t, binary_numbers, res = ord(
+            '0'), {}, 0, [2**i for i in range(0, 10)], 0
+        data[0] = -1
+        for index, digit in enumerate(s):
+            m = ord(digit)-ord_0
+            t ^= binary_numbers[m]
+            if t not in data:
+                data[t] = index
+            if not t:
+                res = max(res, index+1)
+            else:
+                for num in binary_numbers:
+                    tmp = t ^ num
+                    if tmp in data:
+                        res = max(res, index - data[tmp])
+        return res
+
+
