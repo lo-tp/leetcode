08e1ed931a9f48bcb8e48d99dec9bbdf05c04459
lo-tp <regesteraccount@hotmail.com>
2020-10-19 09:29:37 +0800

    1595 done better

--- dp/1595MinimumCosttoConnectTwoGroupsofPoints.py
+++ dp/1595MinimumCosttoConnectTwoGroupsofPoints.py
@@ -17,3 +17,18 @@ class Solution(object):
                         dp[next_state], dp[state]+cost[v][index], tmp[state]+cost[v][index])
             dp, tmp = tmp, dp
         return tmp[-1]
+    def connectTwoGroups(self, cost):
+        sz1, sz2 = len(cost), len(cost[0])
+        max_state, binary_numbers = 1 << sz2, [
+            1 << i for i in range(0, sz2)]
+        tmp = [maxint]*max_state
+        tmp[0] = 0
+        for v in range(0, sz1):
+            dp = [maxint]*max_state
+            for state in range(0, max_state):
+                for j, binary_number in enumerate(binary_numbers):
+                    next_state = state | binary_number
+                    dp[next_state] = min(dp[next_state],
+                                         tmp[state]+cost[v][j], dp[state]+cost[v][j])
+            tmp = dp
+        return tmp[-1]
