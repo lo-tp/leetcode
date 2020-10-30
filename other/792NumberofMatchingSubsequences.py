38bc58bafae44d0c207c2b7f1082194963008c68
lo-tp <regesteraccount@hotmail.com>
2020-10-30 22:26:36 +0800

    792 done

--- other/792NumberofMatchingSubsequences.py
+++ other/792NumberofMatchingSubsequences.py
@@ -12,3 +12,16 @@ class Solution(object):
                 i += 1
             res += 1 if j == w_sz else 0
         return res
+    def numMatchingSubseq(self, S, words):
+        data, res, ord_a = [[] for _ in xrange(0, 26)], 0, ord('a')
+        for w in words:
+            data[ord(w[0])-ord_a].append(w)
+        for i in S:
+            t = ord(i)-ord_a
+            for _ in xrange(0, len(data[t])):
+                w = data[t].pop(0)
+                if len(w) == 1:
+                    res += 1
+                else:
+                    data[ord(w[1])-ord_a].append(w[1:])
+        return res
