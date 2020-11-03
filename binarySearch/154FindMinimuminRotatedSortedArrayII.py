52e334cca0b205a19bc7e5e6d76c824a5c4d8fc0
lo-tp <regesteraccount@hotmail.com>
2020-11-03 09:18:14 +0800

    154 done better

--- binarySearch/154FindMinimuminRotatedSortedArrayII.py
+++ binarySearch/154FindMinimuminRotatedSortedArrayII.py
@@ -33,3 +33,21 @@ class Solution(object):
                 elif nums[m] == nums[l]:
                     l += 1
         return nums[l]
+    def findMin(self, nums):
+        l, r = 0, len(nums)-1
+        if r:
+            while l < r and nums[l] >= nums[r]:
+                m = l+(r-l)/2
+                # m+1 won't overlow because we are not searching for the right bound
+                if nums[m+1] < nums[m]:
+                    return nums[m+1]
+                # m-1 won't overlow because if nums[0] is the one then we wouldn't be in this loop
+                if nums[m] < nums[m-1]:
+                    return nums[m]
+                if nums[m] > nums[l]:
+                    l = m+1
+                elif nums[m] < nums[l]:
+                    r = m-1
+                else:
+                    l += 1
+        return nums[l]
