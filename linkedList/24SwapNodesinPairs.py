4e1ebb8699cd982d69783035a252edb80e4d0636
lo-tp <regesteraccount@hotmail.com>
2020-11-04 19:26:39 +0800

    24 done better

--- linkedList/24SwapNodesinPairs.py
+++ linkedList/24SwapNodesinPairs.py
@@ -1,4 +1,36 @@
+def reverse(head, k):
+    l, r = head, head.next
+    for i in range(0, k):
+        t = r.next
+        r.next = l
+        l, r = r, t
+    return (l, head, r)
+
+def isReversable(head,k):
+    for i in range(0,k):
+        if head:
+            head=head.next
+        else:
+            return False
+    return True
+
 class Solution(object):
+    def reverseKGroup(self, head, k):
+        te=k
+        k -= 1
+        l = head
+        if isReversable(head,te):
+            head, r, next_node = reverse(head, k)
+            while True:
+                l = next_node
+                if isReversable(l, te):
+                    l, r1, t = reverse(next_node, k)
+                    r.next, next_node = l, t
+                    r = r1
+                else:
+                    r.next = next_node
+                    break
+        return head
     def swapPairs(self, head):
         res = head
         if res and res.next:
@@ -17,3 +49,5 @@ class Solution(object):
                     node.next = tmp
                     tmp = prev_node
         return res
+    def swapPairs(self, head):
+        return self.reverseKGroup(head,2)
