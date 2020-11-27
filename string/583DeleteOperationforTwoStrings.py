1f9cf1ecbc3a9e6f5780b35f64ed4089a8ebf335
lo-tp <regesteraccount@hotmail.com>
2020-11-26 20:52:24 -0600

    583 done better

--- string/583DeleteOperationforTwoStrings.py
+++ string/583DeleteOperationforTwoStrings.py
@@ -9,3 +9,14 @@ class Solution(object):
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
         return sz1+sz2-2*dp[sz1][sz2]
+
+    def minDistance(self, text1, text2):
+        sz1, sz2 = len(text1), len(text2)
+        dp = [0]*(sz2+1)
+        for i in range(0, sz1):
+            tmp = [0]
+            for j in range(0,  sz2):
+                tmp.append(dp[j]+1 if text1[i] == text2[j]
+                           else max(tmp[j], dp[j+1]))
+            dp = tmp
+        return sz1+sz2 - 2*dp[-1]
