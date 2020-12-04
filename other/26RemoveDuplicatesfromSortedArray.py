be0c65a1cba191a48b617e5e5697d7eff20ec3a4
lo-tp <regesteraccount@hotmail.com>
2020-12-03 21:32:02 -0600

    26 done in another way

--- other/26RemoveDuplicatesfromSortedArray.py
+++ other/26RemoveDuplicatesfromSortedArray.py
@@ -1,4 +1,31 @@
 class Solution(object):
+    def removeDuplicatesGeneralWay(self, nums, k):
+        res = sz = len(nums)
+        if sz > k:
+            t, count = nums[0], 1
+            for i in range(1, sz):
+                if nums[i] != t:
+                    t, count = nums[i], 1
+                elif count == k:
+                    nums[i] = None
+                else:
+                    count += 1
+            t = 0
+            while t < sz and nums[t] != None:
+                t += 1
+            res = t
+            te = t+1
+            while t < sz and te < sz:
+                while te < sz and nums[te] == None:
+                    te += 1
+                if te < sz:
+                    nums[t] = nums[te]
+                    nums[te] = None
+                    te += 1
+                    res += 1
+                    t += 1
+        return res
+
     def removeDuplicates(self, nums):
         sz, res = len(nums), 0
         if sz:
@@ -10,6 +37,7 @@ class Solution(object):
                     res += 1
                 r += 1
         return res
+
     def removeDuplicates(self, nums):
         sz, slow, fast = len(nums), 1, 1
         while fast < sz:
@@ -19,3 +47,5 @@ class Solution(object):
             fast += 1
         return slow
 
+    def removeDuplicates(self, nums):
+        return self.removeDuplicatesGeneralWay(nums, 1)
