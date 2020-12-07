9f83f15457107eb719c0f3f22ff6853a01264d70
lo-tp <regesteraccount@hotmail.com>
2020-12-07 09:19:37 +0800

    correct 312

--- dp/312BurstBalloons.py
+++ dp/312BurstBalloons.py
@@ -1,19 +1,17 @@
 class Solution(object):
     def maxCoins(self, nums):
-        data=[1]+nums+[1]
-        size=len(nums)
-        dp=[[0 for i in xrange(0, size+2)] for k in xrange(0, size+2)]
-        for i in xrange(1, size+1):
-            dp[i][i]=data[i-1]*data[i]*data[i+1]
-        for interval in xrange(1, size):
-            for startIndex in xrange(1, size+1-interval):
-                endIndex=startIndex+interval
-                for breakPoint in xrange(startIndex, endIndex+1):
-                    dp[startIndex][endIndex]=max(dp[startIndex][endIndex],
-                                                 dp[startIndex][breakPoint-1]+dp[breakPoint+1][endIndex]+data[breakPoint]*data[startIndex-1]*data[breakPoint+1])
-        return dp[1][size]
-s=Solution()
-print s.maxCoins([3,1,5,8])
-print s.maxCoins([3,1])
-print s.maxCoins([3])
-print s.maxCoins([])
+        data = [1]+nums+[1]
+        sz = len(nums)
+        dp = [[0 for i in xrange(0, sz+2)]for k in xrange(0, sz+2)]
+        for i in xrange(1, sz+1):
+            dp[i][i] = data[i-1]*data[i]*data[i+1]
+        for interval in xrange(1, sz):
+            for startIndex in xrange(1, sz - interval+1):
+                endIndex = startIndex+interval
+                for lastIndex in xrange(startIndex, endIndex+1):
+                    dp[startIndex][endIndex] = max(dp[startIndex][endIndex], dp[startIndex][lastIndex-1] +
+                                                   dp[lastIndex+1][endIndex]+data[startIndex-1]*data[lastIndex]*data[endIndex+1])
+        return dp[1][sz] if sz else 0
+
+
+s = Solution()
