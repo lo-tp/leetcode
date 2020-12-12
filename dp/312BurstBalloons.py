b33c98e39447040bec7908bd09b203d71985dcb4
lo-tp <regesteraccount@hotmail.com>
2020-12-12 15:22:13 +0800

    312 done better

--- dp/312BurstBalloons.py
+++ dp/312BurstBalloons.py
@@ -13,5 +13,13 @@ class Solution(object):
                                                    dp[lastIndex+1][endIndex]+data[startIndex-1]*data[lastIndex]*data[endIndex+1])
         return dp[1][sz] if sz else 0
 
-
-s = Solution()
+    def maxCoins(self, nums):
+        nums = [1]+nums+[1]
+        sz = len(nums)
+        dp = [[0 for _ in range(0, sz)] for _ in range(0, sz)]
+        for i in range(sz-3, -1, -1):
+            for j in range(i+2, sz):
+                for t in range(i+1, j):
+                    dp[i][j] = max(dp[i][j], dp[i][t]+nums[i] *
+                                   nums[t]*nums[j] + dp[t][j])
+        return dp[0][sz-1]
