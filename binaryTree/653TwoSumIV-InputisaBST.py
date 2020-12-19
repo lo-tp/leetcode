cc515235e6f1594782b9ea2dfd06dc03ddf8f11a
lo-tp <regesteraccount@hotmail.com>
2020-12-19 13:47:53 -0600

    653 done better

--- binaryTree/653TwoSumIV-InputisaBST.py
+++ binaryTree/653TwoSumIV-InputisaBST.py
@@ -15,4 +15,26 @@ class Solution(object):
                 else:
                     stack.append((root, True))
                     root = root.right
+       return False
+
+class Solution(object):
+    def findTarget(self, root, k):
+        data, stack = [], []
+        while stack or root:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop()
+                data.append(root.val)
+                root = root.right
+        l, r = 0, len(data)-1
+        while l < r:
+            t = data[l]+data[r]
+            if t > k:
+                r -= 1
+            elif t < k:
+                l += 1
+            else:
+                return True
         return False
