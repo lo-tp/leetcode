7c59ad4319fcd99bcaa23ac9b419531892315bc3
lo-tp <regesteraccount@hotmail.com>
2020-12-24 22:34:06 +0800

    140 done better

--- backtracking/140WordBreakII.py
+++ backtracking/140WordBreakII.py
@@ -46,3 +46,12 @@ class Solution(object):
             data[0] = []
         return [sub(' $', '', i) for i in data[0]]
 
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        tmp = set("".join(wordDict))
+        if any([i not in tmp for i in s]):
+            return []
+        dp = [[""], [s[0]]*(s[0] in wordDict)]
+        for i in range(1, len(s)):
+            dp.append(sum([[f"{k} {s[j: i+1]}" if k else s[j: i+1] for k in dp[j]] for j in range(i+1) if s[j: i+1] in wordDict and dp[j]], []))
+        return dp[-1]
