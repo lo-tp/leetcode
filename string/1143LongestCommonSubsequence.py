3f9fe6293453d001ec1619306827e30ba5f60785
lo-tp <regesteraccount@hotmail.com>
2021-01-04 09:49:36 -0600

    1143 done better

--- string/1143LongestCommonSubsequence.py
+++ string/1143LongestCommonSubsequence.py
@@ -9,3 +9,13 @@ class Solution(object):
                 else:
                     data[i][j] = max(data[i][j-1], data[i-1][j], key=len)
         return len(data[-1][-1])
+
+    def longestCommonSubsequence(self, text1, text2):
+        sz1, sz2 = len(text1), len(text2)
+        tmp, dp = [0]*(sz2+1), [0]*(sz2+1)
+        for i in range(0, sz1):
+            for j in range(0,  sz2):
+                tmp[j] = dp[j] + \
+                    1 if text1[i] == text2[j] else max(tmp[j], dp[j+1])
+            dp = tmp
+        return dp[-1]
