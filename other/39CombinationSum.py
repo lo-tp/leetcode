9fdb1dfd02e4bef45ac5ec00ceed34426d82cdb3
lo-tp <regesteraccount@hotmail.com>
2021-01-22 22:50:17 +0800

    39 done better

--- other/39CombinationSum.py
+++ other/39CombinationSum.py
@@ -12,3 +12,21 @@ class Solution(object):
         res=[]
         findCombinations(candidates, len(candidates), target, 0, [], 0, res)
         return res
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        current, sz, res, total = [], len(candidates), [], 0
+        stack = [(i, False) for i in range(0, sz)]
+        while stack:
+            index, visited = stack.pop()
+            if visited:
+                if total == target:
+                    res.append(current[:])
+                total -= candidates[index]
+                current.pop()
+            else:
+                total += candidates[index]
+                current.append(candidates[index])
+                stack.append((index, True))
+                if total < target:
+                    stack.extend([(i, False) for i in range(index, sz)])
+        return res
