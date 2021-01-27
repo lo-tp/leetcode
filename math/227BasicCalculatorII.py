12d4f69be84f04b157cb58f707a0c6bd9170b8e5
lo-tp <regesteraccount@hotmail.com>
2021-01-27 13:21:52 +0800

    227 done

--- math/227BasicCalculatorII.py
+++ math/227BasicCalculatorII.py
@@ -39,3 +39,46 @@ class Solution(object):
                 elif t == '/':
                     stack.append(int(l/r))
         return stack[0]
+    def calculate(self, s):
+        # remove spaces
+        s = compile(' ').sub('', s)
+        operator_reg = compile('^[+/*-]$')
+        token, operator = [], []
+        priorities = {
+            '+': 1,
+            '-': 1,
+            '*': 2,
+            '/': 2,
+            '(': 0,
+        }
+        for t in compile('[+/*-]|[()]|\d+').findall(s):
+            if operator_reg.match(t):
+                while operator and priorities[t] <= priorities[operator[-1]]:
+                    token.append(operator.pop())
+                operator.append(t)
+            elif t == '(':
+                operator.append('(')
+            elif t == ')':
+                while operator[-1] != '(':
+                    token.append(operator.pop())
+                operator.pop()
+            else:
+                token.append(t)
+        token.extend(operator[::-1])
+        stack = []
+        num_reg = compile('\d+')
+        for t in token:
+            if num_reg.match(t):
+                stack.append(int(t))
+            else:
+                r = stack.pop()
+                l = stack.pop()
+                if t == '-':
+                    stack.append(l-r)
+                if t == '+':
+                    stack.append(l+r)
+                elif t == '*':
+                    stack.append(l*r)
+                elif t == '/':
+                    stack.append(int(l/r))
+        return stack[0]
