8a6f95227f3608a5dd7ce9706f61360ab958426f
lo-tp <regesteraccount@hotmail.com>
2021-02-01 09:57:45 -0600

    241 done better

--- backtracking/241DifferentWaystoAddParentheses.py
+++ backtracking/241DifferentWaystoAddParentheses.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from re import split
 
 
 class Solution(object):
@@ -44,4 +45,33 @@ class Solution(object):
                             for w in seen[(i+1, r)]:
                                 seen[(l, r)].append(j-w)
         return seen[(0, sz)]
+
+    def diffWaysToCompute(self, input):
+        numbers = [int(i) for i in split('\D+', input)]
+        if len(numbers) > 1:
+            operators = split('\d+', input)[1:-1]
+            data, stack = defaultdict(list), [(0, len(numbers), False)]
+            while stack:
+                l, r, flag = stack.pop()
+                if not flag:
+                    if not data[(l, r)]:
+                        if l+1 == r:
+                            data[(l, r)].append(numbers[l])
+                        else:
+                            stack.append((l, r, True))
+                            for i in range(l+1, r):
+                                stack.append((l, i, False))
+                                stack.append((i, r, False))
+                else:
+                    for i in range(l+1, r):
+                        for j in data[(l, i)]:
+                            for w in data[(i, r)]:
+                                if operators[i-1] == '+':
+                                    data[(l, r)].append(j+w)
+                                elif operators[i-1] == '-':
+                                    data[(l, r)].append(j-w)
+                                else:
+                                    data[(l, r)].append(j*w)
+            return data[(0, len(numbers))]
+        return [int(input)]
 
