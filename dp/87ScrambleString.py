46e2c51d91c4007f9868b1f6095c30c42977b5cf
lo-tp <regesteraccount@hotmail.com>
2021-02-04 10:31:28 -0600

    87 done better

--- dp/87ScrambleString.py
+++ dp/87ScrambleString.py
@@ -72,3 +72,31 @@ class Solution(object):
                         stack.extend([(i, j, t, False), (i+t, j+t, k-t, False),
                                       (i, j+k-t, t, False), (i+t, j, k-t, False)])
         return dp[0][0][sz1]
+    def isScramble(self, s1, s2):
+        sz = len(s1)
+        dp, seen, stack = [[[False for _ in range(0, sz+1)]for _ in range(
+            0, sz)] for _ in range(0, sz)], set(), [(0, 0, sz, False)]
+        while stack:
+            start1, start2, sz, flag = stack.pop()
+            if flag:
+                if sz == 1:
+                    dp[start1][start2][1] = s1[start1] == s2[start2]
+                else:
+                    for t in range(1, sz):
+                        if (dp[start1][start2][t] and dp[start1+t][start2+t][sz-t]) or (dp[start1][start2+sz-t][t] and dp[start1+t][start2][sz-t]):
+                            dp[start1][start2][sz] = True
+                            break
+                seen.add((start1, start2, sz))
+            elif (start1, start2, sz) not in seen:
+                str1, str2 = s1[start1:start1+sz], s2[start2:start2+sz]
+                if str1 == str2:
+                    dp[start1][start2][sz] = True
+                    seen.add((start1, start2, sz))
+                if sorted(str1) == sorted(str2):
+                    stack.append((start1, start2, sz, True))
+                    for t in range(1, sz):
+                        stack.extend([(start1, start2, t, False), (start1+t, start2+t, sz-t, False),
+                                      (start1, start2+sz-t, t, False), (start1+t, start2, sz-t, False)])
+                else:
+                    seen.add((start1, start2, sz))
+        return dp[0][0][sz]
