a273e003ed85c7e3299e19a51af1729eb760204d
lo-tp <regesteraccount@hotmail.com>
2021-02-08 19:44:52 +0800

    98 done with backtracking

--- binarySearchTree/98ValidateBinarySearchTree.py
+++ binarySearchTree/98ValidateBinarySearchTree.py
@@ -1,4 +1,5 @@
 from sys import maxint
+from sys import maxsize
 
 
 class Solution(object):
@@ -15,4 +16,24 @@ class Solution(object):
                     return False
                 low_end = max(low_end, root.val)
                 root = root.right
+        return True
+
+    def isValidBST(self, root):
+        hi, low, stack = -maxsize, maxsize, [(root, 0, None)]
+        while stack:
+            root, flag, t = stack.pop()
+            if not root:
+                hi, low = -maxsize, maxsize
+            elif not flag:
+                stack.append((root, 1, None))
+                stack.append((root.left, 0, None))
+            elif flag:
+                if root.val <= hi:
+                    return False
+                stack.append((root, 2, root.val if low == maxsize else low))
+                stack.append((root.right, 0, None))
+            else:
+                if root.val >= low:
+                    return False
+                low, hi = t, root.val if hi == -maxsize else hi
         return True
