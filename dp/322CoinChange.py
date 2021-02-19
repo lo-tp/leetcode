dff931ccc0f0618a388bc8888cbfcbe659795844
lo-tp <regesteraccount@hotmail.com>
2021-02-19 12:18:42 -0600

    322 done with DP of top down

--- dp/322CoinChange.py
+++ dp/322CoinChange.py
@@ -9,3 +9,19 @@ class Solution(object):
                     else:
                         dp[i] = dp[i-k]+1
         return dp[amount] if amount in dp else -1
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [-1]*(amount+1)
+        dp[0] = 0
+        stack = [(amount, False)]
+        while stack:
+            t, flag = stack.pop()
+            if dp[t] == -1:
+                if not flag:
+                    stack.append((t, True))
+                    stack.extend([(t-c, False)
+                                  for c in coins if t >= c and dp[t-c] == -1])
+                else:
+                    tmp = [dp[t-c]
+                           for c in coins if t >= c and dp[t-c] != None]
+                    dp[t] = min(tmp)+1 if tmp else None
+        return dp[amount] if dp[amount] != None else -1
