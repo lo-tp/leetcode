6707b11184f938acfe43719554372e10568b2906
lo-tp <regesteraccount@hotmail.com>
2021-02-21 15:15:15 +0800

    518 done

--- dp/518CoinChange2.py
+++ dp/518CoinChange2.py
@@ -1,5 +1,4 @@
 from typing import List
-from collections import defaultdict
 
 
 class Solution:
@@ -20,3 +19,13 @@ class Solution:
         else:
             res = 1
         return res
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = tmp = [0]*(amount+1)
+        dp[0] = tmp[0] = 1
+        for i in range(0, len(coins)):
+            for j in range(1, amount+1):
+                tmp[j] = dp[j]
+                if j-coins[i-1] >= 0:
+                    tmp[j] += tmp[j-coins[i-1]]
+        dp, tmp = tmp, dp
+        return dp[amount]
