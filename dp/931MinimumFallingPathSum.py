d03db7ae6f24291f3d148cdebe293f811973aeff
lo-tp <regesteraccount@hotmail.com>
2021-02-25 19:42:57 +0800

    931 done but WA

---
+++ dp/931MinimumFallingPathSum.py
@@ -0,0 +1,16 @@
+from typing import List
+from sys import maxsize
+
+
+class Solution:
+    def minFallingPathSum(self, m: List[List[int]]) -> int:
+        sz = len(m)
+        dp, tmp = [maxsize]*(sz+2), [maxsize]*(sz+2)
+        for i in range(0, sz):
+            dp[i+1] = m[-1][i]
+
+        for i in range(sz-2, -1, -1):
+            for j in range(1, sz+1):
+                tmp[j] = m[i][j-1]+min(dp[j-1], dp[j+1], dp[j])
+            dp = tmp
+        return min(dp)
