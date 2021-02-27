5ca9c3604a7ae8efb34ac7b2a4e003cd880613d0
lo-tp <regesteraccount@hotmail.com>
2021-02-27 21:37:38 +0800

    514 done better

--- dp/514FreedomTrail.py
+++ dp/514FreedomTrail.py
@@ -19,3 +19,23 @@ class Solution(object):
                     dp[target] = min(new_cur_step, dp[target])
                     stack.append((index, target+1, new_cur_step))
         return dp[-1]
+    def findRotateSteps(self, ring, key):
+        data, r_sz, k_sz = defaultdict(lambda: []), len(ring), len(key)
+        stack, dp = [(0, 0, False)], defaultdict(lambda: maxsize)
+        for index, k in enumerate(ring):
+            data[k].append(index)
+            if k == key[-1]:
+                dp[(index, k_sz)] = 0
+        while stack:
+            cur_r, cur_k, flag = stack.pop()
+            if flag:
+                for index in data[key[cur_k]]:
+                    step = abs(cur_r-index)
+                    step = min(step, r_sz-step)
+                    dp[(cur_r, cur_k)] = min(
+                        dp[(cur_r, cur_k)], step+1+dp[(index, cur_k+1)])
+            elif dp[(cur_r, cur_k)] == maxsize:
+                stack.append((cur_r, cur_k, True))
+                stack.extend([(index, cur_k+1, False)
+                              for index in data[key[cur_k]]])
+        return dp[(0, 0)]
