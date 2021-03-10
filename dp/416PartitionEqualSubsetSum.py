353e3e9afb74c72e7f0fdc37ebf92c8f4c10c135
lo-tp <regesteraccount@hotmail.com>
2021-03-10 11:30:18 +0800

    416 better

--- dp/416PartitionEqualSubsetSum.py
+++ dp/416PartitionEqualSubsetSum.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution(object):
     def canPartition(self, nums):
         total = sum(nums)
@@ -14,5 +16,18 @@ class Solution(object):
                         if j+nums[i] <= target:
                             tmp[j+nums[i]] = True
                 dp = tmp
+            return dp[-1]
+        return False
+    def canPartition(self, nums: List[int]) -> bool:
+        target = sum(nums)
+        if not target % 2:
+            target = int(target/2)
+            dp, tmp = [False]*(target+1), [False]*(target+1)
+            dp[0] = tmp[0] = True
+            for num in nums:
+                for i in range(1, target+1):
+                    te = i-num
+                    tmp[i] = dp[i] or (te >= 0 and dp[te])
+                tmp, dp = dp, tmp
             return dp[-1]
         return False
