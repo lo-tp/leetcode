2ef1a6f4fe257a216193fec7d4b7cfc240675bdb
lo-tp <regesteraccount@hotmail.com>
2021-03-12 11:31:52 -0600

    931 done better

--- dp/931MinimumFallingPathSum.py
+++ dp/931MinimumFallingPathSum.py
@@ -14,3 +14,15 @@ class Solution:
                 tmp[j] = m[i][j-1]+min(dp[j-1], dp[j+1], dp[j])
             dp = tmp
         return min(dp)
+
+    def minFallingPathSum(self, m: List[List[int]]) -> int:
+        sz = len(m)
+        dp, tmp = [maxsize]*(sz+2), [maxsize]*(sz+2)
+        for i in range(0, sz):
+            dp[i+1] = m[-1][i]
+
+        for i in range(sz-2, -1, -1):
+            for j in range(1, sz+1):
+                tmp[j] = m[i][j-1]+min(dp[j-1], dp[j+1], dp[j])
+            tmp, dp = dp, tmp
+        return min(dp)
