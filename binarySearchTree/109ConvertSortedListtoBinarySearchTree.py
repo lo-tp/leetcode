a1373ded73f58c571bbb3f6240f5dfcbc3020ba1
lo-tp <regesteraccount@hotmail.com>
2021-03-23 18:37:05 +0800

    109 done better

--- binarySearchTree/109ConvertSortedListtoBinarySearchTree.py
+++ binarySearchTree/109ConvertSortedListtoBinarySearchTree.py
@@ -27,3 +27,29 @@ class Solution(object):
             data.append(head.val)
             head = head.next
         return sortedArrayToBST(data)
+
+    def sortedListToBST(self, head):
+        res, h, sz = None, head, -1
+        while head:
+            head = head.next
+            sz += 1
+        if sz:
+            stack = [(0, sz, None, 0)]
+            while stack:
+                l, r, node, flag = stack.pop()
+                if r >= l:
+                    m = int(l+(r-l)/2)
+                    if not flag:
+                        stack.append((l, r, node, 1))
+                        stack.append((l, m-1, None, 0))
+                    elif flag == 1:
+                        node = TreeNode(h.val)
+                        h = h.next
+                        node.left = res
+                        stack.append((l, r, node, 2))
+                        stack.append((m+1, r, None, 0))
+                    else:
+                        node.right = res
+                res = node
+        return res
+
