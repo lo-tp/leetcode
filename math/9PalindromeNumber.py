ff10a923bd0997d8bb87ef526d4fc780216f8cc2
lo-tp <regesteraccount@hotmail.com>
2021-05-23 08:59:00 -0600

    9 done in another way but wrong answer

--- math/9PalindromeNumber.py
+++ math/9PalindromeNumber.py
@@ -6,3 +6,12 @@ class Solution:
             if string[i] != string[sz-1-i]:
                 return False
         return True
+    def isPalindrome(self, x: int) -> bool:
+        t = 0
+        while x > t:
+            residue = x % 10
+            t = t*10+residue
+            x = int((x-residue)/10)
+        if x == t:
+            return True
+        return x == int(t/10)
