13b632a02512c95be51dd9a52d7f89fa65819948
lo-tp <regesteraccount@hotmail.com>
2022-06-23 15:53:53 -0600

    300 done better

--- dp/300LongestIncreasingSubsequence.py
+++ dp/300LongestIncreasingSubsequence.py
@@ -1,12 +1,28 @@
 from sys import maxint
-from bisect import bisect_left
+from typing import List
+from math import floor
+
+
+def bisect_left(arr: List[int], target: int):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = floor(left + (right - left) / 2)
+        if arr[mid] < target:
+            left = mid + 1
+        elif arr[mid] > target:
+            right = mid - 1
+        elif arr[mid] == target:
+            right = mid - 1
+    return left
+
+
 class Solution(object):
     def lengthOfLIS(self, nums):
         dp = [1 for i in nums]
         for i in xrange(1, len(nums)):
             for j in xrange(0, i):
                 if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j]+1)
+                    dp[i] = max(dp[i], dp[j] + 1)
         return max(dp) if dp else 0
 
     # time complexity: nlogn
@@ -28,13 +44,13 @@ class Solution(object):
                     records[0] = i
                 else:
                     s = 0
-                    e = ret-1
+                    e = ret - 1
                     while s <= e:
-                        m = s+(e-s)/2
+                        m = s + (e - s) / 2
                         if m == 0 or i > records[m]:
-                            s = m+1
-                        elif i <= records[m-1]:
-                            e = m-1
+                            s = m + 1
+                        elif i <= records[m - 1]:
+                            e = m - 1
                         else:
                             records[m] = i
                             break
@@ -48,5 +64,4 @@ class Solution(object):
             else:
                 j = bisect_left(dp, i)
                 dp[j] = i
-        return len(dp)-1
-
+        return len(dp) - 1
