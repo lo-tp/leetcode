133faf82716c9783d933604536257167b3567c19
lo-tp <regesteraccount@hotmail.com>
2022-06-25 19:51:10 -0600

    464 done

--- dp/464CanIWin.py
+++ dp/464CanIWin.py
@@ -1,38 +1,33 @@
 from typing import List
-from sys import maxsize
-
 from functools import reduce
 
 
 class Solution:
     def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:
         dp = {}
+        dp[0] = True
         allOptions = [i for i in range(1, maxChoosableInteger + 1)]
-        stack = [(allOptions, desiredTotal, 0, tuple(allOptions))]
-        count=100
-        while stack and count:
-            options, total, index, key = stack.pop()
+
+        def dpFunc(options: List[int], total: int):
+            key = tuple(options)
             if options[-1] >= total:
                 dp[key] = True
-            elif sum(options) == total:
+            summation = sum(options)
+            if summation == total:
                 dp[key] = True if len(options) % 2 else False
-            elif sum(options) < total:
+            elif summation < total:
                 dp[key] = False
-            else:
-                nextOptions, nextTotal = (
-                    options[:index] + options[index + 1 :],
-                    total - options[index],
-                )
-                nextKey = tuple(nextOptions)
-                if nextKey in dp:
-                    if not dp[nextKey]:
+            elif key not in dp:
+                dp[key] = False
+                for index in range(0, len(options)):
+                    nextOptions, nextTotal = (
+                        options[:index] + options[index + 1 :],
+                        total - options[index],
+                    )
+                    if not dpFunc(nextOptions, nextTotal):
                         dp[key] = True
-                    elif index < len(options) - 1:
-                        stack.append((options, total, index + 1, key))
-                    else:
-                        dp[key] = False
-                else:
-                    stack.append((options, total, index, key))
-                    stack.append((nextOptions, nextTotal, 0, nextKey))
-            count-=1
-        return dp[tuple(allOptions)]
+                        break
+            return dp[key]
+
+        return dpFunc(allOptions, desiredTotal)
+
