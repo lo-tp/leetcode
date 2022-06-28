fe75a1924aec2b7acdb0c080f3ad5d7eeef00b29
lo-tp <regesteraccount@hotmail.com>
2022-06-28 12:52:23 -0600

    556 done better

--- other/556NextGreaterElementIII.py
+++ other/556NextGreaterElementIII.py
@@ -1,3 +1,7 @@
+from heapq import heappush
+from math import floor
+from bisect import bisect_left
+
 class Solution(object):
     def nextGreaterElement(self, n):
         ord_0, seen, data = ord('0'), [0]*10, list(str(n))
@@ -24,4 +28,27 @@ class Solution(object):
                     if res < 1 << 31:
                         return res
                     return -1
+        return -1
+
+
+    def nextGreaterElement(self, n: int) -> int:
+        nums = []
+        while n:
+            nums.append(-(n % 10))
+            n = floor(n / 10)
+        heap = []
+        for i in range(0, len(nums)):
+            if heap and heap[0] < nums[i]:
+                heap.sort()
+                index = bisect_left(heap, nums[i]) - 1
+                heap[index], nums[i] = nums[i], heap[index]
+                heap.sort()
+                nums = [str(-j) for j in heap + nums[i:]]
+                nums.reverse()
+                res = int("".join(nums))
+                if res <2 ** 31:
+                    return res
+                else:
+                    return -1
+            heappush(heap, nums[i])
         return -1
