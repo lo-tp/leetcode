5bf0500c529140025cc380b804f4aad131207f70
lo-tp <regesteraccount@hotmail.com>
2022-06-28 10:54:33 -0600

    907 done

--- dp/907SumofSubarrayMinimums.py
+++ dp/907SumofSubarrayMinimums.py
@@ -3,7 +3,7 @@ from sys import maxsize
 
 
 class Solution:
-    def sumSubarrayMins(self, arr: List[int]) -> int:
+    def sumSubarrayMinsTLE(self, arr: List[int]) -> int:
         dp = [maxsize]
         res = 0
         for i in arr:
@@ -13,3 +13,17 @@ class Solution:
             dp = tmp
             res += (sum(dp) - maxsize) % (10**9 + 7)
         return res % (10**9 + 7)
+
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        dp = [arr[0]]
+        monoStack = [(arr[0], 0)]
+        for i in range(1, len(arr)):
+            while monoStack and monoStack[-1][0] > arr[i]:
+                monoStack.pop()
+            dp.append(
+                dp[monoStack[-1][1]] + arr[i] * (i - monoStack[-1][1])
+                if monoStack
+                else (i + 1) * arr[i]
+            )
+            monoStack.append((arr[i],i))
+        return sum(dp) % (10**9 + 7)
