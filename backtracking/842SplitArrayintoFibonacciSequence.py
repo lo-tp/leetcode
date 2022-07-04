5efb6ccac50adc265ec2350001fb4259a58076d0
lo-tp <regesteraccount@hotmail.com>
2022-07-04 13:49:19 -0600

    842 done

--- backtracking/842SplitArrayintoFibonacciSequence.py
+++ backtracking/842SplitArrayintoFibonacciSequence.py
@@ -1,38 +1,45 @@
 from typing import List
 
+maxNum = 2**31 - 1
+
 
 def isValidString(num: str):
-    return not (len(num) > 1 and num[0] == "0")
+    if (len(num) > 1 and num[0] == "0") or len(num) > 10:
+        return False
+    elif int(num) > maxNum:
+        return False
+    return True
 
 
 class Solution:
     def splitIntoFibonacci(self, num: str) -> List[int]:
         sz, res = len(num), []
-        for len1 in range(1, min(sz, 10)):
-            for len2 in range(1, min(sz - len1, 10)):
-                num1Str, num2Str = num[:len1], num[len1 : len1 + len2]
-                if isValidString(num1Str) and isValidString(num2Str):
-                    num1, num2 = int(num1Str), int(num2Str)
-                    index = len1 + len2
-                    potentialAnswer = [num1Str, num2Str]
-                    isCorrect = True
-                    while True:
-                        num3 = num1 + num2
-                        num3Str = str(num3)
-                        len3 = len(num3Str)
-                        nextIndex = index + len3
-                        if nextIndex > sz:
-                            isCorrect = False
-                            break
-                        nextStr = num[index:nextIndex]
-                        if isValidString(nextStr) and nextStr == num3Str:
-                            potentialAnswer.append(num3)
-                            num1, num2 = num2, num3
-                            if nextIndex == sz:
-                                return potentialAnswer
-                            index = nextIndex
-                        else:
-                            isCorrect = False
-                            break
+        if sz < 201:
+            for len1 in range(1, min(sz, 10)):
+                for len2 in range(1, min(sz - len1, 10)):
+                    num1Str, num2Str = num[:len1], num[len1 : len1 + len2]
+                    if isValidString(num1Str) and isValidString(num2Str):
+                        num1, num2 = int(num1Str), int(num2Str)
+                        index = len1 + len2
+                        potentialAnswer = [num1Str, num2Str]
+                        isCorrect = True
+                        while True:
+                            num3 = num1 + num2
+                            num3Str = str(num3)
+                            len3 = len(num3Str)
+                            nextIndex = index + len3
+                            if nextIndex > sz:
+                                isCorrect = False
+                                break
+                            nextStr = num[index:nextIndex]
+                            if isValidString(nextStr) and nextStr == num3Str:
+                                potentialAnswer.append(num3)
+                                num1, num2 = num2, num3
+                                if nextIndex == sz:
+                                    return potentialAnswer
+                                index = nextIndex
+                            else:
+                                isCorrect = False
+                                break
         return []
 
