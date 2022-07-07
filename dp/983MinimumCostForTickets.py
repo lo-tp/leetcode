80f50f66b9aa7155fb4fa68396280878c0531802
lo-tp <regesteraccount@hotmail.com>
2022-07-07 08:41:51 -0600

    983 done

---
+++ dp/983MinimumCostForTickets.py
@@ -0,0 +1,26 @@
+from typing import List, Optional
+from math import floor
+from copy import copy
+from functools import reduce
+from sys import maxsize
+from bisect import bisect_right
+
+
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        sz = len(days)
+        if sz == 1:
+            return min(costs)
+        days = [day - days[0] + 1 for day in days]
+        days.insert(0, 0)
+        lastDay = days[-1]
+        dp = [maxsize for _ in days]
+        dp[0] = 0
+        for i in range(1, sz + 1):
+            dp[i] = dp[i - 1] + min(costs)
+            for j in range(1, i):
+                if days[j] + 6 >= days[i]:
+                    dp[i] = min(dp[i], dp[j - 1] + costs[1])
+                if days[j] + 29 >= days[i]:
+                    dp[i] = min(dp[i], dp[j - 1] + costs[2])
+        return dp[-1]
