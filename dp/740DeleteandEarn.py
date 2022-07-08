767c07f556219821d8265337f5704e52a88f79e6
lo-tp <regesteraccount@hotmail.com>
2022-07-08 11:55:34 -0600

    740 done

--- dp/740DeleteandEarn.py
+++ dp/740DeleteandEarn.py
@@ -1,7 +1,6 @@
 from typing import List
 from collections import Counter
 
-
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         u, d = list(set(nums)), Counter(nums)
@@ -12,5 +11,7 @@ class Solution:
         dp = [0] * (sz + 1)
         dp[sz - 1] = u[-1] * d[u[-1]]
         for i in range(sz - 2, -1, -1):
-            dp[i] = u[i] * d[u[i]] + (dp[i + 1] if u[i] + 1 != u[i + 1] else dp[i + 2])
+            dp[i] = u[i] * d[u[i]] + max(
+                dp[i + 1 :] if u[i] + 1 != u[i + 1] else dp[i + 2 :]
+            )
         return max(dp[0], dp[1])
