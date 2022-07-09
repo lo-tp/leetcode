f3da122f62d263ad830e73ca0f04b30387e7e68f
lo-tp <regesteraccount@hotmail.com>
2022-07-09 14:32:02 -0600

    343 done

---
+++ dp/343IntegerBreak.py
@@ -0,0 +1,27 @@
+from typing import List
+from math import floor
+
+
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        dp = [[0] * (n + 1) for i in range(0, n + 1)]
+        stack = [(n, i, False) for i in range(2, n + 1)]
+        while stack:
+            num, divider, flag = stack.pop()
+            if num == divider:
+                dp[num][divider] = 1
+            elif divider == 1:
+                dp[num][divider] = num
+            elif dp[num][divider] == 0:
+                if flag:
+                    t = divider - 1
+                    dp[num][divider] = max(
+                        [k * dp[num - k][t] for k in range(2, num + 2 - divider)]
+                    )
+                else:
+                    stack.append((num, divider, True))
+                    t = divider - 1
+                    stack.extend(
+                        [(num - k, t, False) for k in range(2, num + 2 - divider)]
+                    )
+        return max(dp[num])
