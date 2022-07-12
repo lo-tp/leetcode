92dd1d91e032f2bf4a1fa5de05acb6703a2a5a99
lo-tp <regesteraccount@hotmail.com>
2022-07-12 12:07:44 -0600

    786 done better

--- heap/786K-thSmallestPrimeFraction.py
+++ heap/786K-thSmallestPrimeFraction.py
@@ -5,13 +5,14 @@ from heapq import heappush, heappop
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
         heap = []
-        for i in arr:
-            for j in arr:
-                if i != j:
-                    t = -i / j
-                    if len(heap) < k:
-                        heappush(heap,(t, i, j))
-                    elif t > heap[0][0]:
-                        heappop(heap)
-                        heappush(heap,(t, i, j))
-        return [heap[0][1], heap[0][2]]
+        sz = len(arr)
+        for i in range(1, sz):
+            heappush(heap, (arr[0] / arr[i], 0, i))
+        k -= 1
+        while k:
+            _, i, j = heappop(heap)
+            if i + 1 < j:
+                i += 1
+                heappush(heap, (arr[i] / arr[j], i, j))
+            k -= 1
+        return [arr[heap[0][1]], arr[heap[0][2]]]
