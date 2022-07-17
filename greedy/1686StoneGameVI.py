826b3859194a74178328484aef6b568634045e82
lo-tp <regesteraccount@hotmail.com>
2022-07-16 19:15:14 -0600

    1686 done better

--- greedy/1686StoneGameVI.py
+++ greedy/1686StoneGameVI.py
@@ -3,17 +3,18 @@ from typing import List
 
 class Solution:
     def stoneGameVI(self, aliceValues: List[int], bobValues: List[int]) -> int:
-        s = [(-aliceValues[i] - bobValues[i], i) for i in range(0, len(aliceValues))]
-        s.sort()
-        aPoint = bPoint = 0
-        for i in range(0, len(aliceValues)):
-            if i % 2:
-                bPoint += bobValues[s[i][1]]
-            else:
-                aPoint += aliceValues[s[i][1]]
-        t = aPoint - bPoint
-        if t > 0:
-            return 1
-        elif t < 0:
+        t = sorted(
+            [
+                (-aliceValues[i] - bobValues[i], aliceValues[i], bobValues[i])
+                for i in range(0, len(aliceValues))
+            ]
+        )
+        res = reduce(
+            lambda accu, i: accu - (t[i][2] if i % 2 else -t[i][1]), range(0, len(t)), 0
+        )
+        if res < 0:
             return -1
-        return 0
+        elif res > 0:
+            return 1
+        return res
+
