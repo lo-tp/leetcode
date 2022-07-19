ffd9ac8284391c5b14c8ba3ca22f47d092a1fd5f
lo-tp <regesteraccount@hotmail.com>
2022-07-19 10:55:47 -0600

    feat: 162 done

--- binarySearch/162FindPeakElement.py
+++ binarySearch/162FindPeakElement.py
@@ -1,22 +1,21 @@
 from typing import List
-from math import floor
-from functools import cache
+from sys import maxsize
 
 
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         sz = len(nums)
-        if sz == 1 or nums[0] > nums[1]:
-            return nums[0]
+        def get(index: int):
+            if index==-1 or index==sz:
+                return -maxsize
+            return nums[index]
         l, r = 0, sz - 1
         while l <= r:
             m = l + floor((r - l) / 2)
-            if not m or m == sz - 1:
+            if get(m-1)<get(m)>get(m+1):
                 return m
-            elif nums[m - 1] > nums[m]:
-                r = m - 1
-            elif nums[m + 1] > m:
-                l = m + 1
+            elif get(m-1)>get(m):
+                r=m-1
             else:
-                return m
+                l=m+1
 
