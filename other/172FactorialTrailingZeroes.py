82578a30e76ce6f08162463c1eb74b9d03d3b307
lo-tp <regesteraccount@hotmail.com>
2022-07-20 12:35:50 -0600

    172 done

---
+++ other/172FactorialTrailingZeroes.py
@@ -0,0 +1,19 @@
+from typing import List, Optional
+from math import floor
+
+
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        res = 0
+        for i in range(5, n + 1):
+            if not i % 10:
+                while not i % 10:
+                    # print(i)
+                    res += 1
+                    i = floor(i / 10)
+            if not i % 5:
+                while not i % 5:
+                    # print(i)
+                    res += 1
+                    i = floor(i / 5)
+        return res
