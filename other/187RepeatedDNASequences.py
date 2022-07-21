52fcaf7b4a378d7b8037b5487f51f99148b4a5c2
lo-tp <regesteraccount@hotmail.com>
2022-07-21 13:48:10 -0600

    187 done with bit mask

--- other/187RepeatedDNASequences.py
+++ other/187RepeatedDNASequences.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+from collections import defaultdict
 
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
@@ -9,3 +10,26 @@ class Solution:
                 res.add(t)
             seen[t] = True
         return list(res)
+
+
+    def findRepeatedDnaSequencesBetter(self, s: str) -> List[str]:
+        sz = len(s)
+        res = []
+        if sz > 10:
+            mapping = {"A": 0, "C": 1, "G": 2, "T": 3}
+            state = 0
+            validNum = (1 << 20) - 1
+            for i in range(0, 10):
+                state <<= 2
+                state |= mapping[s[i]]
+
+            seen = defaultdict(lambda: 0)
+            seen[state] += 1
+            for i in range(10, sz):
+                state <<= 2
+                state |= mapping[s[i]]
+                state &= validNum
+                if seen[state] == 1:
+                    res.append(s[i - 9 : i + 1])
+                seen[state] += 1
+        return res
