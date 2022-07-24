87a9bd650f77b3c4f11f2279542b00750fb79b9c
lo-tp <regesteraccount@hotmail.com>
2022-07-24 13:55:21 -0600

    837 done with

--- dp/837New21Game.py
+++ dp/837New21Game.py
@@ -20,3 +20,13 @@ class Solution:
                         goodGame += prob
                 else:
                     stack.extend([(i, False) for i in range(1, maxPts)])
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        dp = [1] * (n + 1)
+        totalProb = 0
+        for i in range(1, n + 1):
+            if i <= k:
+                totalProb += dp[i - 1]
+            if k + maxPts >= i > maxPts:
+                totalProb -= dp[i - maxPts - 1]
+            dp[i] = totalProb / (maxPts)
+        return sum(dp[k:])
