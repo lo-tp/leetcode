80ae0fb1040820b74038f1535158815d53be96d3
lo-tp <regesteraccount@hotmail.com>
2022-07-25 11:59:41 -0600

    229 done with Boyer-Moore Majority Vote algorithm

--- other/229MajorityElementII.py
+++ other/229MajorityElementII.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 from collections import defaultdict
-from math import ceil
+from math import ceil, floor
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
@@ -10,4 +10,21 @@ class Solution:
             counter[num] += 1
             if counter[num] == sz:
                 res.append(num)
-        return num
+        return res
+
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        sz = floor(len(nums) / 3)
+        candidate1, candidate2, count1, count2 = 0, 1, 0, 0
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+            elif not count1:
+                candidate1, count1 = num, 1
+            elif not count2:
+                candidate2, count2 = num, 1
+            else:
+                count1 -= 1
+                count2 -= 1
+        return [n for n in [candidate1, candidate2] if nums.count(n) > sz]
