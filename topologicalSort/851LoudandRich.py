10d652b0916a623cca2760be340fc09f7eab8467
lo-tp <regesteraccount@hotmail.com>
2022-08-03 21:40:49 -0600

    851 done

--- topologicalSort/851LoudandRich.py
+++ topologicalSort/851LoudandRich.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Solution:
     def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
         sz = len(quiet)
@@ -7,7 +8,7 @@ class Solution:
         graph = [[] for _ in range(0, sz)]
         for target, source in richer:
             graph[source].append(target)
-        res = [0] * sz
+        res = [-1] * sz
         for i in range(0, sz):
             stack = [(i, False)]
             res[i] = i
@@ -19,6 +20,13 @@ class Solution:
                         q = quiet[index]
                         res[i] = index
                 else:
-                    stack.append((index, True))
-                    stack.extend([(i, False) for i in graph[index]])
+                    if not visited[index]:
+                        stack.append((index, True))
+                        stack.extend([(i, False) for i in graph[index]])
+                    else:
+                        if quiet[res[index]] < q:
+                            q = quiet[res[index]]
+                            res[i] = res[index]
+            visited[i] = True
         return res
+
