72cb43e59a14290ba4c36b20f16bad24921bf0bf
lo-tp <regesteraccount@hotmail.com>
2022-08-16 14:28:10 -0600

    547 done better

--- unionFind/547FriendCircles.py
+++ unionFind/547FriendCircles.py
@@ -1,50 +1,37 @@
-class UF():
-    def __init__(self, size):
-        self.root = [-1]*size
-        self.count = 0
-
-    def find(self, target):
-        if self.root[target] != -1:
-            tmp = target
-            while tmp != self.root[tmp]:
-                tmp = self.root[tmp]
-            return tmp
-        return -1
-
-    def merge(self, a, b):
-        root_a, root_b = self.find(a), self.find(b)
-        if root_a == -1 and root_b == -1:
-            self.root[a] = self.root[b] = b
-            self.count += 1
-        elif root_a == -1:
-            self.root[a] = root_b
-        elif root_b == -1:
-            self.root[b] = root_a
-        elif a != b and root_a != root_b:
-            self.root[root_a] = root_b
-            self.count -= 1
+from typing import List
 
 
-class Solution(object):
-    def findCircleNumBFS(self, M):
-        res, size = 0, len(M)
-        stack, visited = [i for i in xrange(0, size)], [False]*size
-        while stack:
-            index = stack.pop()
-            if not visited[index]:
-                res += 1
-                tmp = [index]
-                while tmp:
-                    i = tmp.pop()
-                    visited[i] = True
-                    tmp.extend([k for k in xrange(0, size)
-                                if not visited[k] and M[i][k]])
-        return res    def findCircleNum(self, M):
-        size = len(M)
-        uf = UF(size)
-        for column_index in xrange(0, size):
-            for row_index in xrange(0, size):
-                if M[column_index][row_index]:
-                    uf.merge(column_index, row_index)
-        return uf.count
+class Union:
+    def __init__(self, sz: int):
+        self.sz = sz
+        self.weight = [0] * sz
+        self.parent = [i for i in range(0, sz)]
 
+    def count(self):
+        return self.sz
+
+    def find(self, n: int):
+        while n != self.parent[n]:
+            self.parent[n] = self.parent[self.parent[n]]
+            n = self.parent[n]
+        return n
+
+    def union(self, x: int, y: int):
+        pX, pY = self.find(x), self.find(y)
+        if pX != pY:
+            if self.weight[pX] < self.weight[pY]:
+                pX, pY = pY, pX
+            self.parent[pY] = pX
+            self.weight[pX] += self.weight[pY]
+            self.sz -= 1
+
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        sz = len(isConnected)
+        u = Union(sz)
+        for x in range(0, sz):
+            for y in range(0, sz):
+                if isConnected[x][y]:
+                    u.union(x, y)
+        return u.count()
