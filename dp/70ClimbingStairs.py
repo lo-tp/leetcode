5a63764ba61944520c4aa6fef8d2e19b971b5dfb
lo-tp <regesteraccount@hotmail.com>
2026-07-09 09:13:08 -0600

    feat: refine 70

--- dp/70ClimbingStairs.py
+++ dp/70ClimbingStairs.py
@@ -10,3 +10,17 @@ class Solution(object):
         for i in xrange(2, n):
             prev1, prev2 = prev2, prev1+prev2
         return prev2
+    def find_ways(self, n:int):
+        if not n in self.cache:
+            self.cache[n]=self.find_ways(n-1)+self.cache[n]+=self.find_ways(n-2)
+        return self.cache[n]
+        
+        
+
+    def climbStairs_dp_recursive(self, n: int) -> int:
+        self.cache={}
+        self.cache[1]=1
+        self.cache[0]=1
+        self.cache[-1]=1
+        return self.find_ways(n)
+
