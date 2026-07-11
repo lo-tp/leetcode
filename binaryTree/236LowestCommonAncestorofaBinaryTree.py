2fb7e3b4f2813c9cbf27b70063bbc60aaef499cd
lo-tp <regesteraccount@hotmail.com>
2026-07-11 16:43:13 -0600

    feat: refine 236 with an recursion

--- binaryTree/236LowestCommonAncestorofaBinaryTree.py
+++ binaryTree/236LowestCommonAncestorofaBinaryTree.py
@@ -61,3 +61,10 @@ class Solution(object):
             q=parent[q]
         return q
 
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root==p or root==q:
+            return root
+        l,r=self.lowestCommonAncestor(root.left, p,q), self.lowestCommonAncestor(root.right, p,q)
+        if l and r:
+            return root
+        return l if l else r
