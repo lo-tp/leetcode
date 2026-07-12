547f13a70ea2949ab01329b3b31a1f338889ae96
lo-tp <regesteraccount@hotmail.com>
2026-07-12 15:07:06 -0600

    feat: refine 222

--- binaryTree/222CountCompleteTreeNodes.py
+++ binaryTree/222CountCompleteTreeNodes.py
@@ -5,6 +5,19 @@ def getHeight(node: TreeNode) -> int:
         node = node.left
     return res
 
+def getNodeCount(root):
+    if root:
+        height,l,r=1,root.left, root.right
+        while l and r:
+            height+=1
+            r,l=r.right, l.left
+        if l or r:
+            return getNodeCount(root.left)+getNodeCount(root.right)+1
+        else:
+            return 2**height-1
+    return 0
+
+
 
 class Solution(object):
     def countNodes(self, root):
@@ -42,3 +55,6 @@ class Solution(object):
                 res += (1 << r_height)-1
                 root = root.left
         return res
+
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        return getNodeCount(root)
