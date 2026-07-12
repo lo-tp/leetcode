994804b62566e1af2f0890034fd4301fc0308217
lo-tp <regesteraccount@hotmail.com>
2026-07-11 22:23:08 -0600

    feat: refine 52

--- backtracking/52N-QueensII.py
+++ backtracking/52N-QueensII.py
@@ -31,3 +31,26 @@ class Solution(object):
                                   for i in xrange(0, sz) if not remain[index][i]])
         return res
 
+
+    def totalNQueens(self, n: int) -> int:
+        res,horizontal_index, index_sum, index_diff=0,set(),set(),set()
+        stack=[(0,i, False) for i in range(0,n)]
+        while stack:
+            i,j, visited=stack.pop()
+            if visited:
+                horizontal_index.remove(j)
+                index_sum.remove(i+j)
+                index_diff.remove(i-j)
+            else:
+                if len(horizontal_index)==n-1:
+                    res+=1
+                    continue
+                horizontal_index.add(j)
+                index_sum.add(i+j)
+                index_diff.add(i-j)
+                stack.append((i, j, True))
+                i+=1
+                for t in range range(0,n):
+                    if not t in horizontal_index and not i+t in index_sum and not i-t in index_diff:
+                        stack.append((i,t,False))
+        return res
