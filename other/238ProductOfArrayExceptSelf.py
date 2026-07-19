112a01b21b2b01856cb9f3c9a88ec924b9e0c661
lo-tp <regesteraccount@hotmail.com>
2026-07-19 10:54:38 -0600

    feat: 283 done

--- other/238ProductOfArrayExceptSelf.py
+++ other/238ProductOfArrayExceptSelf.py
@@ -14,5 +14,28 @@ class Solution(object):
             left*=nums[x]
         return ret
 
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        sz=len(nums)
+        zero_count=0
+        zero_idx=-1
+        t=1
+        for idx,n in enemerate(nums):
+            if not n:
+                zero_count+=1
+                zero_idx=idx
+        if zero_count>1:
+            return [0]*sz
+        if zero_count:
+            for n in nums:
+                t*=n if n else 1
+            return [t if idx==zero_idx else 0 for idx in range(0,sz)]
+        res=[]
+        for idx in range(0, sz-1):
+            t*=nums[idx]
+        for idx in range(0,sz-1):
+            res.append(t/nums[idx]*nums[-1])
+        res.append(t)
+        return res
+
 s=Solution()
 print s.productExceptSelf( [1,2])
