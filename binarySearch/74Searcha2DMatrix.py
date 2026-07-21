a86c1b3209db706a80f76dda9a8b6fc750c17422
lo-tp <regesteraccount@hotmail.com>
2026-07-21 07:50:36 -0600

    feat: 74 done better

--- binarySearch/74Searcha2DMatrix.py
+++ binarySearch/74Searcha2DMatrix.py
@@ -23,3 +23,19 @@ class Solution(object):
                         return True
         return False
 
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        v_sz, h_sz=len(matrix), len(matrix[0])
+        l,r=0, v_sz*h_sz-1
+        def getIndexes(n):
+            return (n//h_sz, n%h_sz)
+        while l<=r:
+            mid=l+(r-l)//2
+            mid_v, mid_h=getIndexes(mid)
+            if matrix[mid_v][mid_h]==target:
+                return True
+            elif matrix[mid_v][mid_h]>target:
+                r=mid-1
+            else:
+                l=mid+1
+        return False
