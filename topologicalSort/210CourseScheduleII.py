c00c13f9039dd740b19e4b5e73bbc52bb241208e
lo-tp <regesteraccount@hotmail.com>
2026-07-22 11:14:44 -0600

    feat: 210 done with bfs

--- topologicalSort/210CourseScheduleII.py
+++ topologicalSort/210CourseScheduleII.py
@@ -17,3 +17,31 @@ class Solution(object):
                     stack.append(next)
 
         return res if len(res) == numCourse else []
+
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        in_degree = [0] * numCourses
+        adj_list = [[] for _ in range(numCourses)]
+        
+        # In graph theory, edges go from u -> v. 
+        # Here, u is the prerequisite, and v is the dependent course.
+        for v, u in prerequisites:
+            in_degree[v] += 1
+            adj_list[u].append(v)
+
+        # Nodes with an in-degree of 0 have no incoming edges (no prerequisites)
+        stack = [i for i in range(numCourses) if in_degree[i] == 0]
+        topological_order = []
+        
+        while stack:
+            node = stack.pop()
+            topological_order.append(node)
+            
+            for neighbor in adj_list[node]:
+                in_degree[neighbor] -= 1
+                
+                # If a neighbor's in-degree drops to 0, all its prerequisites are met
+                if in_degree[neighbor] == 0:
+                    stack.append(neighbor)
+                    
+        # If we couldn't process every node, there was a cycle in the graph
+        return topological_order if len(topological_order) == numCourses else []
